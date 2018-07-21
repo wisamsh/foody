e9fa25bfd56b4d9e41cf3ac5dec7efb12ac9efa6
Matan Zari <zari@moveo.co.il>
2018-07-21 18:34:47 +0300

    - content-with-sidebar set to default template for recipe, article and playlist

--- web/app/themes/Foody/functions/post-types.php
+++ web/app/themes/Foody/functions/post-types.php
@@ -13,8 +13,8 @@ function register_post_types()
         'recipe' => array(
             'name' => 'Recipes',
             'singular_name' => 'Recipe',
-	        'taxonomies'=>array('category','post_tag'),
-            'supports' => array( 'title', 'editor', 'thumbnail', 'revisions' ),
+            'taxonomies' => array('category', 'post_tag'),
+            'supports' => array('title', 'editor', 'thumbnail', 'revisions'),
             'show_ui' => true,
         ),
         'accessory' => array(
@@ -37,24 +37,24 @@ function register_post_types()
 
     foreach ($post_types as $type) {
 
-    	$args = array(
-		    'labels' => array(
-			    'name' => __($type['name']),
-			    'singular_name' => __($type['singular_name']),
-		    ),
-		    'public' => true,
-		    'has_archive' => true,
+        $args = array(
+            'labels' => array(
+                'name' => __($type['name']),
+                'singular_name' => __($type['singular_name']),
+            ),
+            'public' => true,
+            'has_archive' => true,
             'capability_type' => 'post'
-	    );
-
-    	if(isset($type['taxonomies'])){
-		    $args['taxonomies'] = $type['taxonomies'];
-	    }
-        register_post_type(strtolower('foody_' . $type['singular_name']),
-	        $args
         );
 
-    	$supported_features = array(
+        if (isset($type['taxonomies'])) {
+            $args['taxonomies'] = $type['taxonomies'];
+        }
+        register_post_type(strtolower('foody_' . $type['singular_name']),
+            $args
+        );
+
+        $supported_features = array(
             'title',
             'editor',
             'author',
@@ -69,7 +69,7 @@ function register_post_types()
         );
 
         foreach ($supported_features as $feature) {
-            add_post_type_support(strtolower('foody_' . $type['singular_name']),$feature);
+            add_post_type_support(strtolower('foody_' . $type['singular_name']), $feature);
         }
 
     }
@@ -77,4 +77,36 @@ function register_post_types()
 
 add_action('init', 'register_post_types');
 
+
+/**
+ *
+ */
+function foody_remove_page_template()
+{
+    global $pagenow;
+
+    $custom_content_post_types = array(
+        'foody_recipe',
+        'foody_article',
+        'foody_playlist'
+    );
+
+    $default_template = 'page-templates/content-with-sidebar.php';
+
+    if (in_array($pagenow, array('post-new.php', 'post.php')) && in_array(get_post_type(), $custom_content_post_types)) {
+        ?>
+        <script type="text/javascript">
+            (function ($) {
+                $(document).ready(function () {
+                    $('#page_template').val('<?php echo $default_template?>');
+                })
+
+
+            })(jQuery)
+        </script>
+        <?php
+    }
+}
+
+add_action('admin_footer', 'foody_remove_page_template', 10);
 
