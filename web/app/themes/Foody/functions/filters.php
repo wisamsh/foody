7f243a4e10b2f1d8e780d9800ccc5d0411ab2d38
Matan Zari <zari@moveo.co.il>
2018-10-02 09:57:40 +0300

    - changed default facebook login permissions

--- web/app/themes/Foody/functions/filters.php
+++ web/app/themes/Foody/functions/filters.php
@@ -64,3 +64,14 @@ function default_page_template($template)
 
 add_filter('template_include', 'default_page_template', 10);
 
+function wsl_change_default_permissons($provider_scope, $provider)
+{
+    if ('facebook' == strtolower($provider)) {
+        $provider_scope = 'email, public_profile';
+    }
+
+    return $provider_scope;
+}
+
+add_filter('wsl_hook_alter_provider_scope', 'wsl_change_default_permissons', 10, 2);
+
