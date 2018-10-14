732adec77eb63173be289a46b8dad26386bc472f
Matan Zari <zari@moveo.co.il>
2018-10-14 22:58:03 +0300

    - medio-dev env

--- config/environments/medio-development.php
+++ config/environments/medio-development.php
@@ -16,3 +16,5 @@ define('FACEBOOK_APP_ID', '300250737432710');
 define('FACEBOOK_API_VERSION', 'v3.1');
 define('GOOGLE_APP_ID', '589726890032-lplp2v2oa8prujvo03jda02b7cramucc.apps.googleusercontent.com');
 define('GOOGLE_CAPTCHA_SECRET', '6Lc7eXIUAAAAAKG_OzJR3AblE0ADojSuiOD10hq5');
+// TODO change to Medio's account
+define('MIXPANEL_TOKEN','4f106af7ed1ff172ef5bf5a29d7af3af');
