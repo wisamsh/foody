4c84cba77aa7d3cd593cd77634183c52580a9393
Matan Zari <zari@moveo.co.il>
2018-10-19 17:12:23 +0300

    - mobile image upload orientation fix

--- web/app/themes/Foody/resources/js/recipe/index.js
+++ web/app/themes/Foody/resources/js/recipe/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by moveosoftware on 6/27/18.
  */
-
+window.EXIF = require('exif-js');
 jQuery(document).ready(function () {
     window.scroller = require('../common/scroll-progress');
 
