6320b0d5c34956a01fca0d7d7b575e4772dc55f3
Matan Zari <zari@moveo.co.il>
2018-11-05 11:25:11 +0200

    - iOS screen lock fix (touch move listener)

--- web/app/themes/Foody/resources/js/common/screenLock.js
+++ web/app/themes/Foody/resources/js/common/screenLock.js
@@ -1,15 +1,22 @@
 /**
  * Created by moveosoftware on 11/5/18.
  */
+document.addEventListener('touchmove', function (event) {
+    let bodyClasses = document.body.classList;
+    if (bodyClasses.indexOf('lock') != -1 || bodyClasses.indexOf('side-active') != -1) {
+        event.preventDefault();
+    }
+}, {passive: false});
+
 module.exports = function (lock, overlay) {
     let lockClasses = 'lock';
-    if(overlay){
+    if (overlay) {
         lockClasses = `${lockClasses} side-active`;
     }
 
-    if(lock){
+    if (lock) {
         jQuery('html,body').addClass(lockClasses);
-    }else{
+    } else {
         jQuery('html,body').removeClass(lockClasses);
     }
 };
