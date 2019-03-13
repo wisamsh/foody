dc3fbc4af9219b83c6fe94f2b8dc41efe0446de7
Matan Zari <zari@moveo.co.il>
2019-03-13 14:18:44 +0200

    - register js errors fix

--- web/app/themes/Foody/resources/js/login/login.js
+++ web/app/themes/Foody/resources/js/login/login.js
@@ -4,7 +4,23 @@
 jQuery(document).ready(($) => {
 
     let $form = $('#login-form');
+    $.validator.addMethod(
+        "regex",
+        function (value, element, regexp) {
+            return this.optional(element) || regexp.test(value);
+        }
+    );
 
+    $.validator.addMethod(
+        "password",
+        function (value) {
+
+            let hasNumbers = /[0-9]+/.test(value);
+            let nonEn = /[^a-z0-9]/i.test(value);
+
+            return hasNumbers && nonEn === false;
+        }
+    );
     $.validator.addMethod(
         "emailOrUsername",
         function (value, element) {
