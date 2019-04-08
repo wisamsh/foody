8b9faa9601b0eeea7b89decac4979aacd9859777
Omer Fishman <omer@moveo.co.il>
2019-04-08 21:05:34 +0300

    Added success & failure events for registration completion.

--- web/app/themes/Foody/resources/js/common/approvals.js
+++ web/app/themes/Foody/resources/js/common/approvals.js
@@ -30,15 +30,68 @@ jQuery(document).ready(($) => {
                         marketing: $('#approvals #check-marketing', $approvalsContainer).prop('checked'),
                         e_book: $('#approvals #check-e-book', $approvalsContainer).prop('checked')
                     }
-                }, function () {
-                    $approvalsContainer.unblock();
-                    let $redirect = $('input[name="redirect"]');
-                    // noinspection EqualityComparisonWithCoercionJS
-                    if ($redirect.length && $redirect.val() == 1) {
-                        window.location.href = '/';
+                }, function (err, data) {
+                    if (err) {
+                        if (err.responseJSON && err.responseJSON.data && err.responseJSON.data[0] && err.responseJSON.data[0].message) {
+                            eventCallback('', 'רישום לאתר', 'רישום לדיוור נכשל', foodyGlobals['user']['social_type'], 'הודעה', err.responseJSON.data[0].message);
+                        }
+                    } else {
+                        let marketingAnalyticsText = 'לא נרשם';
+                        if (data.marketing) {
+                            marketingAnalyticsText = 'נרשם';
+                            if (data.e_book) {
+                                marketingAnalyticsText = 'נרשם פלוס ספר';
+                            }
+                        } else if (data.e_book) {
+                            marketingAnalyticsText = 'לא נרשם פלוס ספר';
+                        }
+                        eventCallback('', 'רישום לאתר', 'רישום לדיוור הצליח', foodyGlobals['user']['social_type'], 'רישום לדיוור', marketingAnalyticsText);
+                        $approvalsContainer.unblock();
+                        let $redirect = $('input[name="redirect"]');
+                        // noinspection EqualityComparisonWithCoercionJS
+                        if ($redirect.length && $redirect.val() == 1) {
+                            window.location.href = '/';
+                        }
                     }
                 });
             }
         });
     }
 });
+
+
+/**
+ * Handle events and fire analytics dataLayer.push
+ * @param event
+ * @param category
+ * @param action
+ * @param label
+ * @param cdDesc
+ * @param cdValue
+ */
+function eventCallback(event, category, action, label, cdDesc, cdValue) {
+
+    /**
+     * Logged in user ID
+     */
+    let customerID = foodyGlobals['loggedInUser'] ? foodyGlobals['loggedInUser'] : '';
+
+    tagManager.pushDataLayer(
+        category,
+        action,
+        label,
+        customerID,
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        cdDesc,
+        cdValue,
+        ''
+    );
+}
