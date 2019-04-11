3dfe511a957166c1e69d4a0b73ede0968191b2b1
Omer Fishman <omer@moveo.co.il>
2019-04-11 16:36:48 +0300

    Fixed social share og:image generate

--- web/app/themes/Foody/functions/seo.php
+++ web/app/themes/Foody/functions/seo.php
@@ -6,35 +6,48 @@
  * Time: 3:17 PM
  */
 
-add_filter('wpseo_robots', 'foody_handle_no_index');
-function foody_handle_no_index($robots_str)
-{
-    $no_index = 'noindex,nofollow';
+add_filter( 'wpseo_robots', 'foody_handle_no_index' );
+function foody_handle_no_index( $robots_str ) {
+	$no_index = 'noindex,nofollow';
 
-    if (is_single()) {
-        $comments_regex = '/comment-page-[0-9]+(\/?)$/';
-        if (isset($_SERVER['REQUEST_URI'])) {
-            $uri = $_SERVER['REQUEST_URI'];
+	if ( is_single() ) {
+		$comments_regex = '/comment-page-[0-9]+(\/?)$/';
+		if ( isset( $_SERVER['REQUEST_URI'] ) ) {
+			$uri = $_SERVER['REQUEST_URI'];
 
-            if (preg_match($comments_regex, $uri) != false) {
-                $robots_str = $no_index;
-            }
-        }
-    } elseif (isset($_GET['redirect_to']) && !empty($_GET['redirect_to'])) {
-        $robots_str = $no_index;
-    }
+			if ( preg_match( $comments_regex, $uri ) != false ) {
+				$robots_str = $no_index;
+			}
+		}
+	} elseif ( isset( $_GET['redirect_to'] ) && ! empty( $_GET['redirect_to'] ) ) {
+		$robots_str = $no_index;
+	}
 
-    return $robots_str;
+	return $robots_str;
 }
 
 
-add_filter('wpseo_metadesc', 'foody_set_meta_desc');
-function foody_set_meta_desc($desc)
-{
-    if (is_tag()) {
-        $name = get_term_field('name', get_queried_object_id());
-        $desc = sprintf("%s - כל המתכונים והכתבות הכי מעניינות וטעימות. פודי, הצטרפו לפודי - קהילת האוכל הגדולה של ישראל.", $name);
-    }
+add_filter( 'wpseo_metadesc', 'foody_set_meta_desc' );
+function foody_set_meta_desc( $desc ) {
+	if ( is_tag() ) {
+		$name = get_term_field( 'name', get_queried_object_id() );
+		$desc = sprintf( "%s - כל המתכונים והכתבות הכי מעניינות וטעימות. פודי, הצטרפו לפודי - קהילת האוכל הגדולה של ישראל.", $name );
+	}
 
-    return $desc;
+	return $desc;
+}
+
+add_action( 'wpseo_opengraph', 'change_yoast_seo_og_meta' );
+function change_yoast_seo_og_meta() {
+	if ( is_single() ) {
+		add_filter( 'wpseo_opengraph_image', 'foody_change_image' );
+	}
+}
+
+
+function foody_change_image( $image ) {
+
+	$image = get_the_post_thumbnail_url();
+
+	return $image;
 }
