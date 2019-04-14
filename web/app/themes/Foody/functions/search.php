5f76e18f9e2a9c2b88ee87ce8d3ddae0e9a07cec
Matan Zari <zari@moveo.co.il>
2019-04-14 11:02:45 +0300

    - default filters for categories, channels, authors, tags

--- web/app/themes/Foody/functions/search.php
+++ web/app/themes/Foody/functions/search.php
@@ -124,8 +124,16 @@ function foody_filters_acf_load_value($value)
 
     if (empty($value)) {
 
-        $post_type = get_post_type();
-        if(is_category() || is_tag() || $post_type == 'foody_channel' || is_author()){
+        $screen = get_current_screen();
+
+        $relevant_screens = [
+            'edit-category',
+            'foody_channel',
+            'user-edit',
+            'edit-post_tag'
+        ];
+
+        if (in_array($screen->id, $relevant_screens)) {
             // no values set for filters lists,
             // load default values from global search settings (foody_search_settings)
             // see Sidebar_Filter for more details.
