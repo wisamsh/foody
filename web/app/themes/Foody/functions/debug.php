6776a9f262f5c1181c0edcd04724a9a82e4e084a
Matan Zari <zari@moveo.co.il>
2019-04-23 10:56:11 +0300

    - changed import task to wp cron

--- web/app/themes/Foody/functions/debug.php
+++ web/app/themes/Foody/functions/debug.php
@@ -6,15 +6,33 @@
  * Time: 4:25 PM
  */
 
-add_action('admin_init', 'foody_debug_admin_actions');
+add_action('wp_dashboard_setup', 'foody_debug_admin_actions');
 
 function foody_debug_admin_actions()
 {
     if (is_multisite() && is_main_site()) {
         try {
-            new Foody_WhiteLabelDuplicatorTask();
+//            new Foody_WhiteLabelDuplicatorTask();
+            function do_this_in_ten_seconds()
+            {
+                $max_execution_time = ini_get('max_execution_time');
+                ini_set('max_execution_time', 300);
+                Foody_WhiteLabelDuplicator::whiteLabelCreate(2);
+                ini_set('max_execution_time', $max_execution_time);
+            }
+
+            add_action('my_debug_event', 'do_this_in_ten_seconds');
+
+            $scheduled = wp_schedule_single_event(time() + 10, 'my_debug_event');
+            $message = 'scheduled import cron';
+            if ($scheduled === false) {
+                $message = 'schedule import cron failed';
+            }
+
+            Foody_WhiteLabelLogger::info($message);
+
         } catch (Exception $e) {
-            Foody_WhiteLabelLogger::error($e->getMessage(),['error'=>$e]);
+            Foody_WhiteLabelLogger::error($e->getMessage(), ['error' => $e]);
         }
     }
 }
