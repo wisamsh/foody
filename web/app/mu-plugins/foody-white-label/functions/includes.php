a87d028878043111c1dacacd4844ea4ec11e8cc2
Omer Fishman <omer@moveo.co.il>
2019-04-24 19:03:18 +0300

    Added Tax-Meta-Class lib Added flags for indexing and passing data to multisite sites on user (author) edit, category & tags edit

--- web/app/mu-plugins/foody-white-label/functions/includes.php
+++ web/app/mu-plugins/foody-white-label/functions/includes.php
@@ -9,6 +9,7 @@
 
 if (is_main_site()){
     require_once plugin_dir_path(__FILE__) . "content-sync.php";
+	require_once plugin_dir_path(__FILE__) . "sites.php";
 }
 
 require_once plugin_dir_path(__FILE__) . "acf.php";
