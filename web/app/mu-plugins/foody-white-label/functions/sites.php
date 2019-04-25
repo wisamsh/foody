741b12e5b5aa4a4f6d6a22c6709a926e2fa12b93
Omer Fishman <omer@moveo.co.il>
2019-04-25 17:10:59 +0300

    - Autoformat file. - Removed Foody main site from sites. - Fixed an issue with undefined user obj.

--- web/app/mu-plugins/foody-white-label/functions/sites.php
+++ web/app/mu-plugins/foody-white-label/functions/sites.php
@@ -7,50 +7,49 @@
  */
 
 
-if (is_admin() && is_main_site()) {
-    require_once PLUGIN_DIR . 'Tax-meta-class/Tax-meta-class.php';
+if ( is_admin() && is_main_site() ) {
+	require_once PLUGIN_DIR . 'Tax-meta-class/Tax-meta-class.php';
 
-    $config = array(
-        'id' => 'site_data_meta_box',
-        'title' => __('העתקת מידע לאתרים', 'foody'),
-        'pages' => array('category', 'post_tag'),
-        'context' => 'normal',
-        'fields' => array(),
-        'local_images' => false,
-        'use_with_theme' => false
-    );
-    $my_meta = new Tax_Meta_Class($config);
-    $args = [];//[ 'site__not_in' => [ get_main_site_id() ] ];
-    $sites = get_sites($args);
+	$config  = array(
+		'id'             => 'site_data_meta_box',
+		'title'          => __( 'העתקת מידע לאתרים', 'foody' ),
+		'pages'          => array( 'category', 'post_tag' ),
+		'context'        => 'normal',
+		'fields'         => array(),
+		'local_images'   => false,
+		'use_with_theme' => false
+	);
+	$my_meta = new Tax_Meta_Class( $config );
+	$args    = [ 'site__not_in' => [ get_main_site_id() ] ];
+	$sites   = get_sites( $args );
 
-    foreach ($sites as $site) {
-        $my_meta->addCheckbox('pass_data_' . $site->id, array('name' => __('העתק ל-' . $site->blogname, 'foody')));
-        $my_meta->addCheckbox('index_data_' . $site->id, array('name' => __('index ל-' . $site->blogname, 'foody')));
-    }
+	foreach ( $sites as $site ) {
+		$my_meta->addCheckbox( 'pass_data_' . $site->id, array( 'name' => __( 'העתק ל-' . $site->blogname, 'foody' ) ) );
+		$my_meta->addCheckbox( 'index_data_' . $site->id, array( 'name' => __( 'index ל-' . $site->blogname, 'foody' ) ) );
+	}
 
-    $my_meta->Finish();
+	$my_meta->Finish();
 }
 
 //$saved_data = get_tax_meta( $term->id, 'pass_data_' . $site->id );
 
 // User
-add_action('show_user_profile', 'foody_profile_edit_action');
-add_action('edit_user_profile', 'foody_profile_edit_action');
-function foody_profile_edit_action($user)
-{
+add_action( 'show_user_profile', 'foody_profile_edit_action' );
+add_action( 'edit_user_profile', 'foody_profile_edit_action' );
+function foody_profile_edit_action( $user ) {
 
-    if (is_admin() && in_array('author', (array)$user->roles)) {
-        $args = ['site__not_in' => [get_main_site_id()]];
-        $sites = get_sites($args);
+	if ( is_admin() && in_array( 'author', (array) $user->roles ) ) {
+		$args  = [ 'site__not_in' => [ get_main_site_id() ] ];
+		$sites = get_sites( $args );
 
-        ?>
+		?>
         <h3>העתקת מתכונים</h3>
-        <?php
-        foreach ($sites as $site) {
+		<?php
+		foreach ( $sites as $site ) {
 
-            $checked = (isset($user->{'pass_data_' . $site->id}) && $user->{'pass_data_' . $site->id}) ? ' checked="checked"' : '';
-            $index_checked = (isset($user->{'index_data_' . $site->id}) && $user->{'index_data_' . $site->id}) ? ' checked="checked"' : '';
-            ?>
+			$checked       = ( isset( $user->{'pass_data_' . $site->id} ) && $user->{'pass_data_' . $site->id} ) ? ' checked="checked"' : '';
+			$index_checked = ( isset( $user->{'index_data_' . $site->id} ) && $user->{'index_data_' . $site->id} ) ? ' checked="checked"' : '';
+			?>
             <label for="pass_data_<?php echo $site->id ?>">
                 <input name="pass_data_<?php echo $site->id ?>" type="checkbox" id="pass_data_<?php echo $site->id ?>"
                        value="1"<?php echo $checked; ?>>
@@ -64,36 +63,36 @@ function foody_profile_edit_action($user)
             </label>
             <br/>
             <br/>
-            <?php
+			<?php
 
-        }
-    }
+		}
+	}
 }
 
-add_action('personal_options_update', 'foody_profile_update_action');
-add_action('edit_user_profile_update', 'foody_profile_update_action');
+add_action( 'personal_options_update', 'foody_profile_update_action' );
+add_action( 'edit_user_profile_update', 'foody_profile_update_action' );
 
-function foody_profile_update_action($user_id)
-{
-    if (is_admin() && in_array('author', (array)$user->roles)) {
+function foody_profile_update_action( $user_id ) {
+	$user = get_user_by( 'id', $user_id );
+	if ( is_admin() && in_array( 'author', (array) $user->roles ) ) {
 
-        $args = ['site__not_in' => [get_main_site_id()]];
-        $sites = get_sites($args);
+		$args  = [ 'site__not_in' => [ get_main_site_id() ] ];
+		$sites = get_sites( $args );
 
-        foreach ($sites as $site) {
-            update_user_meta($user_id, 'pass_data_' . $site->id, isset($_POST['pass_data_' . $site->id]));
-            update_user_meta($user_id, 'index_data_' . $site->id, isset($_POST['index_data_' . $site->id]));
-        }
-    }
+		foreach ( $sites as $site ) {
+			update_user_meta( $user_id, 'pass_data_' . $site->id, isset( $_POST[ 'pass_data_' . $site->id ] ) );
+			update_user_meta( $user_id, 'index_data_' . $site->id, isset( $_POST[ 'index_data_' . $site->id ] ) );
+		}
+	}
 }
 
 //if (get_user_meta($current_user->ID, 'pass_data_' . $site->id, true)) {
 
 
 function foody_is_registration_open() {
-    if (is_multisite()) {
-	    return get_option( 'nsur_join_site_enabled' );
-    } else {
-        return get_option( 'users_can_register' );
-    }
+	if ( is_multisite() ) {
+		return get_option( 'nsur_join_site_enabled' );
+	} else {
+		return get_option( 'users_can_register' );
+	}
 }
