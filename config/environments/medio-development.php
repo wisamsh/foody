5c52f76368e493b3fbde4c60db50cec46e419c87
Matan Zari <zari@moveo.co.il>
2019-05-03 21:29:58 +0300

    - multisite config updates

--- config/environments/medio-development.php
+++ config/environments/medio-development.php
@@ -20,6 +20,8 @@ define('GOOGLE_CAPTCHA_SECRET', '6Lc7eXIUAAAAAKG_OzJR3AblE0ADojSuiOD10hq5');
 define('MIXPANEL_TOKEN', '4f106af7ed1ff172ef5bf5a29d7af3af');
 define('GOOGLE_TAG_MANAGER_ID','GTM-M2SBXJF');
 
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
 define('DOMAIN_CURRENT_SITE', preg_replace('/http(s?):\/\//','',env('WP_HOME')));
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
