647c2a6cc3e6168df7d6cd770d85c6607b1ea432
Omer Fishman <omer@moveo.co.il>
2019-05-22 19:49:44 +0300

    Added settimeout for facebook & nagishli scripts

--- web/app/themes/Foody/inc/classes/class-header.php
+++ web/app/themes/Foody/inc/classes/class-header.php
@@ -44,25 +44,27 @@ class Foody_Header
         ?>
 
         <script async defer>
-            window.fbAsyncInit = function () {
-                FB.init({
-                    appId: '<?php echo FACEBOOK_APP_ID?>',
-                    cookie: true,
-                    xfbml: true,
-                    version: '<?php echo FACEBOOK_API_VERSION ?>'
-                });
-            };
+            setTimeout(() => {
+                window.fbAsyncInit = function () {
+                    FB.init({
+                        appId: '<?php echo FACEBOOK_APP_ID?>',
+                        cookie: true,
+                        xfbml: true,
+                        version: '<?php echo FACEBOOK_API_VERSION ?>'
+                    });
+                };
 
-            (function (d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) {
-                    return;
-                }
-                js = d.createElement(s);
-                js.id = id;
-                js.src = "https://connect.facebook.net/en_US/sdk.js";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
+                (function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) {
+                        return;
+                    }
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = "https://connect.facebook.net/en_US/sdk.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+            });
         </script>
 
         <?php
