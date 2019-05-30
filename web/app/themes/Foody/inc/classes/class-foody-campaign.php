dadb716858b613a49feb8d5f64f1f69b2931f80c
Omer Fishman <omer@moveo.co.il>
2019-05-30 10:38:12 +0300

    Removed campaign defaults

--- web/app/themes/Foody/inc/classes/class-foody-campaign.php
+++ web/app/themes/Foody/inc/classes/class-foody-campaign.php
@@ -48,9 +48,7 @@ class Foody_Campaign {
 
 	public function get_hero_image_url() {
 		$image = get_field( 'hero_image' );
-		if ( empty( $image ) ) {
-			$image = $GLOBALS['images_dir'] . 'e-book.png';
-		} else {
+		if ( ! empty( $image ) ) {
 			$image = $image['url'];
 		}
 
@@ -59,9 +57,7 @@ class Foody_Campaign {
 
 	public function get_mobile_hero_image_url() {
 		$image = get_field( 'mobile_hero_image' );
-		if ( empty( $image ) ) {
-			$image = $GLOBALS['images_dir'] . 'e-book-mobile.png';
-		} else {
+		if ( ! empty( $image ) ) {
 			$image = $image['url'];
 		}
 
@@ -82,27 +78,9 @@ class Foody_Campaign {
 
 	public function get_promoted_images() {
 		$images = get_field( 'images' );
-		if ( empty( $images ) ) {
-			$images = [
-				[
-					'image'    => [ 'url' => $GLOBALS['images_dir'] . 'pavlove.png' ],
-					'title'    => 'פבלובה של מיקי שמו',
-					'subtitle' => 'עוגת מרנג עם קציפת וניל ופירות',
-				],
-				[
-					'image'    => [ 'url' => $GLOBALS['images_dir'] . 'kinoa.png' ],
-					'title'    => 'סלט קינואה של שר פיטנס',
-					'subtitle' => 'סלט קינואה עם עשבי תיבול,חמוציות ואגוזים',
-				],
-				[
-					'image'    => [ 'url' => $GLOBALS['images_dir'] . 'salmon.png' ],
-					'title'    => 'סלמון של יונית צוקרמן',
-					'subtitle' => 'דג סלמון ברוטב מרוקאי חריף',
-				]
-			];
+		if ( ! empty( $images ) ) {
+			return $images;
 		}
-
-		return $images;
 	}
 
 }
