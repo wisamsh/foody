e01dfcfca8a948c4841910212340e3a5416b33dc
Omer Fishman <omer@moveo.co.il>
2019-06-11 21:40:01 +0300

    REadd w3tc

--- config/environments/medio-development.php
+++ config/environments/medio-development.php
@@ -27,3 +27,4 @@ define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
 define( 'WP_DEFAULT_THEME', 'Foody');
+define( 'W3TC_CONFIG_DIR', '/home/ubuntu/efs-test/staging-test/w3tc-config' );
