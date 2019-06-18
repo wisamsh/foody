d98affc70b2724d2f8575da9e454a2c1da2c8869
Matan Zari <zari@moveo.co.il>
2019-06-18 12:07:59 +0300

    - force assistant font on quad menu to eliminate google font loading

--- web/app/themes/Foody/page-templates/homepage.php
+++ web/app/themes/Foody/page-templates/homepage.php
@@ -45,7 +45,7 @@ $homepage = new Foody_HomePage();
 
                 <section id="homepage-main-content" class="content-container col-lg-9 col-12">
 
-<!--                    --><?php //$homepage->feed(); ?>
+                    <?php $homepage->feed(); ?>
 
                 </section>
 
