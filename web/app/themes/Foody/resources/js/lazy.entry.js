f13a5ad7245f1f15502cd605ccbf1ae639f2d977
Matan Zari <zari@moveo.co.il>
2019-06-18 16:24:06 +0300

    - changed lazy loading mode

--- web/app/themes/Foody/resources/js/lazy.entry.js
+++ web/app/themes/Foody/resources/js/lazy.entry.js
@@ -1,7 +1,7 @@
 window.lazySizesConfig = window.lazySizesConfig || {};
 
 window.lazySizesConfig.srcAttr = 'data-foody-src';
-window.lazySizesConfig.loadMode = 1;
+window.lazySizesConfig.loadMode = 3;
 // window.lazySizesConfig.preloadClass = 'shine lazypreload';
 // window.lazySizesConfig.loadingClass = 'shine lazyloading';
 require('lazysizes');
