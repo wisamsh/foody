d96c29d6e1753e49dbdd7e164e70cca33085172a
Matan Zari <zari@moveo.co.il>
2019-06-18 16:15:15 +0300

    - lazy loading before animations and image blur

--- web/app/themes/Foody/functions/foody-head.php
+++ web/app/themes/Foody/functions/foody-head.php
@@ -330,3 +330,35 @@ function foody_replace_to_webp()
     </script>
     <?php
 }
+
+add_action('wp_head', 'foody_add_shimmer_styles');
+function foody_add_shimmer_styles()
+{
+    ?>
+    <style>
+        .shine:after {
+
+            background: #f6f7f8 linear-gradient(to right, #f6f7f8 0%, #edeef1 20%, #f6f7f8 40%, #f6f7f8 100%) no-repeat;
+            background-size: 355px 236px;
+            display: block;
+            position: relative;
+            -webkit-animation-duration: 1s;
+            -webkit-animation-fill-mode: forwards;
+            -webkit-animation-iteration-count: infinite;
+            -webkit-animation-name: placeholderShimmer;
+            -webkit-animation-timing-function: linear;
+        }
+
+        @-webkit-keyframes placeholderShimmer {
+            0% {
+                background-position: -355px 0;
+            }
+
+            100% {
+                background-position: 355px 0;
+            }
+        }
+
+    </style>
+    <?php
+}
--- web/app/themes/Foody/resources/js/lazy.entry.js
+++ web/app/themes/Foody/resources/js/lazy.entry.js
@@ -2,4 +2,6 @@ window.lazySizesConfig = window.lazySizesConfig || {};
 
 window.lazySizesConfig.srcAttr = 'data-foody-src';
 window.lazySizesConfig.loadMode = 1;
+// window.lazySizesConfig.preloadClass = 'shine lazypreload';
+// window.lazySizesConfig.loadingClass = 'shine lazyloading';
 require('lazysizes');
