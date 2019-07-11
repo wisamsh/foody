91bec2515c880680733cf9d701190b641cc3cc3d
Omer Fishman <omer@moveo.co.il>
2019-07-11 18:53:10 +0300

    added foody course page template

--- web/app/themes/Foody/inc/classes/class-foody-course.php
+++ web/app/themes/Foody/inc/classes/class-foody-course.php
@@ -20,36 +20,57 @@ class Foody_Course {
 		return $cover_image;
 	}
 
-	public function get_main_video() {
-		$video = get_field( 'video' );
-
-		return $video;
-	}
-
-	public function get_floating_button() {
+	public function get_floating_registration_button() {
 		$floating_registration_button = get_field( 'floating_registration_button' );
 
 		return $floating_registration_button;
 	}
 
-	public function the_about() {
-		$about = get_field( 'about' );
-
-		echo $about;
-	}
-
 	public function get_information_top_section() {
 		$information_top_sections = get_field( 'information_top_section' );
 
-		$information_section = [];
+		return $information_top_sections;
+	}
 
-		array_map( function ( $section ) use ( $information_section ) {
-			$information_section[] = array(
-				'title'    => $section['title'],
-				'subtitle' => $section['subtitle']
-			);
-		}, $information_top_sections );
+	public function get_information_bottom_section() {
+		$information_bottom_section = get_field( 'information_bottom_section' );
 
-		return $information_section;
+		return $information_bottom_section;
+	}
+
+	public function get_information_registration_link() {
+		$information_registration_link = get_field( 'information_info_registration_link' );
+
+		return $information_registration_link;
+	}
+
+	public function get_course_is_for_title() {
+		$course_is_for_title = get_field( 'course_is_for_title' );
+
+		return $course_is_for_title;
+	}
+
+	public function get_course_is_for() {
+		$course_is_for_bullets = get_field( 'course_is_for_bullets' );
+
+		return $course_is_for_bullets;
+	}
+
+	public function get_how_it_works_title() {
+		$how_it_works_title = get_field( 'how_it_works_title' );
+
+		return $how_it_works_title;
+	}
+
+	public function get_how_it_works() {
+		$how_it_works_steps = get_field( 'how_it_works_steps' );
+
+		return $how_it_works_steps;
+	}
+
+	public function get_how_it_works_registration_link() {
+		$how_it_works_steps_registration = get_field( 'how_it_works_steps_registration_link' );
+
+		return $how_it_works_steps_registration;
 	}
 }
--- web/app/themes/Foody/page-templates/foody-course.php
+++ web/app/themes/Foody/page-templates/foody-course.php
@@ -13,14 +13,111 @@ $course = new Foody_Course();
 
 ?>
 
+    <!--    <div class="sticky-registration">-->
+    <!--		--><?php
+//		foody_get_template_part( get_template_directory() . '/template-parts/common/link.php', array( 'link' => $course->get_floating_registration_button() ) );
+//		?>
+    <!--    </div>-->
     <div id="main-content" class="main-content">
 
         <div id="primary" class="content-area">
+			<?php
+			$image = $course->get_cover_image();
+			foody_get_template_part( get_template_directory() . '/template-parts/content-cover-image.php', array(
+				'image' => $image,
+				'link'  => ''
+			) );
+			?>
 
-            <div class="course-information-top-section">
+            <div class="cover-video">
+				<?php the_field( 'video' ); ?>
+            </div>
 
-				<?php $course->get_information_top_section(); ?>
+            <div class="about">
+				<?php the_field( 'about' ); ?>
+            </div>
 
+            <div class="course-information">
+
+                <div class="course-information-top-section">
+
+					<?php
+					$information = $course->get_information_top_section();
+					foreach ( $information as $information_item ) {
+						echo '<span class="information-item">';
+						echo '<div class="title information-title">' . $information_item['title'] . '</div>';
+						echo '<div class="information-subtitle">' . $information_item['subtitle'] . '</div>';
+						echo '</span>';
+					}
+					?>
+
+                </div>
+
+                <div class="course-information-bottom-section">
+
+                    <div class="information-item price">
+						<?php
+						$information = $course->get_information_bottom_section();
+						if ( ! empty( $information ) ) {
+							echo '<span class="price-text">' . $information['price_text'] . '</span>';
+							echo '<span class="price-number">' . '₪' . $information['price'] . '</span>';
+						}
+						?>
+                    </div>
+
+                    <span class="information-item information-registration-link">
+                        <?php
+                        foody_get_template_part( get_template_directory() . '/template-parts/common/link.php', array( 'link' => $course->get_information_registration_link() ) );
+                        ?>
+                </div>
+
+            </div>
+
+
+            <div class="course-is-for">
+
+                <div class="title course-is-for-title">
+					<?php echo $course->get_course_is_for_title(); ?>
+                </div>
+                <ul class="course-is-for-bullets">
+
+					<?php
+					$course_is_for_bullets = $course->get_course_is_for();
+					foreach ( $course_is_for_bullets as $bullet ) {
+						echo '<li class="course-is-for-item">';
+						echo '<span class="course-is-for-bullet">' . $bullet['bullet_text'] . '</span>';
+						echo '</li>';
+					}
+					?>
+
+                </ul>
+
+            </div>
+
+            <div class="how-it-works">
+
+                <div class="title how-it-works-title">
+					<?php echo $course->get_how_it_works_title(); ?>
+                </div>
+                <div class="how-it-works-bullets">
+
+					<?php
+					$how_it_works_steps = $course->get_how_it_works();
+					foreach ( $how_it_works_steps as $step ) {
+						echo '<span class="how-it-works-item">';
+						echo '<span class="how-it-works-step-number">' . $step['step_number_text'] . '</span>';
+						echo '<span class="how-it-works-step">' . $step['step_text'] . '</span>';
+						echo '</span>';
+					}
+					?>
+
+                </div>
+
+                <span class="how-it-works-registration-link">
+                    <?php
+                    foody_get_template_part( get_template_directory() . '/template-parts/common/link.php', array( 'link' => $course->get_how_it_works_registration_link() ) );
+                    ?>
+                </span>
             </div>
 
         </div><!-- #primary -->
