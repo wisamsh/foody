134209d5c25ed8d1702b40b6fdc02d7c111a8d2f
Matan Zari <zari@moveo.co.il>
2019-07-16 10:16:51 +0300

    - changed lazy load mode

--- web/app/themes/Foody/resources/js/lazy.entry.js
+++ web/app/themes/Foody/resources/js/lazy.entry.js
@@ -6,7 +6,7 @@ lazySizesConfig.srcAttr = 'data-foody-src';
 lazySizesConfig.preloadAfterLoad = true;
 
 // page is optimized for fast onload event
-// lazySizesConfig.loadMode = 1;
+lazySizesConfig.loadMode = 1;
 
 require('lazysizes');
 // import a plugin
