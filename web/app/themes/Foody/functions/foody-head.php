750f7afd61304cb8fd7bf771b0901dfeaf3b90bb
Matan Zari <zari@moveo.co.il>
2019-07-16 11:01:57 +0300

    - changed lay load mode - removed hotjar

--- web/app/themes/Foody/functions/foody-head.php
+++ web/app/themes/Foody/functions/foody-head.php
@@ -175,21 +175,6 @@ function foody_env_scripts()
 {
     $scripts = [
         'http://foody.moveodevelop.com' => [
-            "    (function(h,o,t,j,a,r){
-
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-
-        h._hjSettings={hjid:1114919,hjsv:6};
-
-        a=o.getElementsByTagName('head')[0];
-
-        r=o.createElement('script');r.async=1;
-
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-
-        a.appendChild(r);
-
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"
         ],
         'https://foody.co.il' => []
     ];
