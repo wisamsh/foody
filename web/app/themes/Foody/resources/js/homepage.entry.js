4896a93c532411dd2698c20572fe09e9e86c7b08
Matan Zari <zari@moveo.co.il>
2019-07-16 18:56:13 +0300

    - lottie issue fix

--- web/app/themes/Foody/resources/js/homepage.entry.js
+++ web/app/themes/Foody/resources/js/homepage.entry.js
@@ -1,6 +1,7 @@
-require('./homepage');
-require('./white-label');
-
 if(! window.lottie ){
     window.lottie = require('lottie-web/build/player/lottie.min');
 }
+
+require('./homepage');
+require('./white-label');
+
