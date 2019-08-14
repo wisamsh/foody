eb94ce14dc4c9210fe46295092e29a9836ab8559
Daniel Kissos <danielk@moveo.co.il>
2019-08-14 16:49:04 +0300

    newsletter- for every length of the required string the message would add new line -category in menu changed color to black (in staging it was white)

--- web/app/themes/Foody/resources/js/common/newsletter.js
+++ web/app/themes/Foody/resources/js/common/newsletter.js
@@ -8,9 +8,14 @@ jQuery(document).ready(($) => {
 
         let messageSelector = $('.invalid .wpcf7-not-valid-tip');
         let responseOutput = $('.invalid .wpcf7-response-output');
+        let newLine = $('<br class="newsletter-new-line">');
+        if($(".newsletter-new-line").length){
+            $(".newsletter-new-line").remove();
+        }
         messageSelector.remove();
         responseOutput.remove();
         messageSelector.insertBefore($('.site-footer .invalid .bmd-form-group'));
+        newLine.insertAfter(messageSelector);
     });
 });
 
