b6d304bc7a03ec321c5dca0cfc37284a31317adb
Daniel Kissos <danielk@moveo.co.il>
2019-10-16 17:54:06 +0300

    israel cookes feature

--- web/app/themes/Foody/functions/user-profile.php
+++ web/app/themes/Foody/functions/user-profile.php
@@ -7,15 +7,33 @@
  */
 
 
-function extra_profile_fields( $user ) { ?>
+function extra_profile_fields($user)
+{ ?>
 
     <h3>השלמת פרטי קמפיין</h3>
     <table class="form-table">
         <tr>
+            <th><label for="type_of_writer">סוג כותב</label></th>
+            <td>
+                <?php if (get_the_author_meta('type_of_writer', $user->ID) == 'israel-cooks') { ?>
+                    <select name="type_of_writer" id="type_of_writer">
+                        <option value="team">נבחרת</option>
+                        <option selected="selected" value="israel-cooks">בשלני ישראל</option>
+                    </select><br/>
+                <?php } else {
+                    ?>
+                    <select name="type_of_writer" id="type_of_writer">
+                        <option selected="selected" value="team">נבחרת</option>
+                        <option value="israel-cooks">בשלני ישראל</option>
+                    </select><br/>
+                <?php } ?>
+            </td>
+        </tr>
+        <tr>
             <th><label for="street">כתובת</label></th>
             <td>
                 <input type="text" name="street" id="street"
-                       value="<?php echo esc_attr( get_the_author_meta( 'street', $user->ID ) ); ?>"
+                       value="<?php echo esc_attr(get_the_author_meta('street', $user->ID)); ?>"
                        class="regular-text"/><br/>
             </td>
         </tr>
@@ -23,7 +41,7 @@ function extra_profile_fields( $user ) { ?>
             <th><label for="street-number">מספר בית</label></th>
             <td>
                 <input type="text" name="street-number" id="street-number"
-                       value="<?php echo esc_attr( get_the_author_meta( 'street_number', $user->ID ) ); ?>"
+                       value="<?php echo esc_attr(get_the_author_meta('street_number', $user->ID)); ?>"
                        class="regular-text"/><br/>
             </td>
         </tr>
@@ -31,7 +49,7 @@ function extra_profile_fields( $user ) { ?>
             <th><label for="city">עיר</label></th>
             <td>
                 <input type="text" name="city" id="city"
-                       value="<?php echo esc_attr( get_the_author_meta( 'city', $user->ID ) ); ?>"
+                       value="<?php echo esc_attr(get_the_author_meta('city', $user->ID)); ?>"
                        class="regular-text"/><br/>
             </td>
         </tr>
@@ -39,44 +57,46 @@ function extra_profile_fields( $user ) { ?>
             <th><label for="birthday">תאריך לידה</label></th>
             <td>
                 <input type="date" name="birthday" id="birthday"
-                       value="<?php echo esc_attr( get_the_author_meta( 'birthday', $user->ID ) ); ?>"
+                       value="<?php echo esc_attr(get_the_author_meta('birthday', $user->ID)); ?>"
                        class="regular-text"/><br/>
             </td>
         </tr>
         <tr>
             <th><label for="gender">מין</label></th>
             <td>
-                <input <?php echo get_the_author_meta( 'gender', $user->ID ) == 'male' ? 'checked' : ''; ?>
+                <input <?php echo get_the_author_meta('gender', $user->ID) == 'male' ? 'checked' : ''; ?>
                         type="radio" name="gender" id="gender" value="male" class="regular-text"/>זכר&nbsp;&nbsp;&nbsp;
-                <input <?php echo get_the_author_meta( 'gender', $user->ID ) == 'female' ? 'checked' : ''; ?>
+                <input <?php echo get_the_author_meta('gender', $user->ID) == 'female' ? 'checked' : ''; ?>
                         type="radio" name="gender" id="gender" value="female" class="regular-text"/>נקבה&nbsp;&nbsp;&nbsp;
-                <input <?php echo get_the_author_meta( 'gender', $user->ID ) == 'other' ? 'checked' : ''; ?>
+                <input <?php echo get_the_author_meta('gender', $user->ID) == 'other' ? 'checked' : ''; ?>
                         type="radio" name="gender" id="gender" value="other" class="regular-text"/>אחר
             </td>
         </tr>
     </table>
-	<?php
+    <?php
 
 }
 
 // Then we hook the function to "show_user_profile" and "edit_user_profile"
-add_action( 'show_user_profile', 'extra_profile_fields', 10 );
-add_action( 'edit_user_profile', 'extra_profile_fields', 10 );
+add_action('show_user_profile', 'extra_profile_fields', 10);
+add_action('edit_user_profile', 'extra_profile_fields', 10);
 
 
-function save_extra_profile_fields( $user_id ) {
+function save_extra_profile_fields($user_id)
+{
 
-	if ( ! current_user_can( 'edit_user', $user_id ) ) {
-		return false;
-	}
+    if (!current_user_can('edit_user', $user_id)) {
+        return false;
+    }
 
-	/* Edit the following lines according to your set fields */
-	update_user_meta( $user_id, 'street', $_POST['street'] );
-	update_user_meta( $user_id, 'street_number', $_POST['street-number'] );
-	update_user_meta( $user_id, 'city', $_POST['city'] );
-	update_user_meta( $user_id, 'birthday', $_POST['birthday'] );
-	update_user_meta( $user_id, 'gender', $_POST['gender'] );
+    /* Edit the following lines according to your set fields */
+    update_user_meta($user_id, 'street', $_POST['street']);
+    update_user_meta($user_id, 'street_number', $_POST['street-number']);
+    update_user_meta($user_id, 'city', $_POST['city']);
+    update_user_meta($user_id, 'birthday', $_POST['birthday']);
+    update_user_meta($user_id, 'gender', $_POST['gender']);
+    update_user_meta($user_id, 'type_of_writer', $_POST['type_of_writer']);
 }
 
-add_action( 'personal_options_update', 'save_extra_profile_fields' );
-add_action( 'edit_user_profile_update', 'save_extra_profile_fields' );
+add_action('personal_options_update', 'save_extra_profile_fields');
+add_action('edit_user_profile_update', 'save_extra_profile_fields');
--- web/app/themes/Foody/functions/users.php
+++ web/app/themes/Foody/functions/users.php
@@ -6,16 +6,62 @@
  * Time: 4:21 PM
  */
 
-function foody_api_user_role() {
-	$role_name = 'foody_api_user';
-	add_role(
-		$role_name,
-		'Foody API User',
-		[ 'read' ]
-	);
+function foody_api_user_role()
+{
+    $role_name = 'foody_api_user';
+    add_role(
+        $role_name,
+        'Foody API User',
+        ['read']
+    );
 
-	$role = get_role( $role_name );
-	$role->add_cap( 'access_foody_api', true );
+    $role = get_role($role_name);
+    $role->add_cap('access_foody_api', true);
 }
 
-add_action( 'init', 'foody_api_user_role' );
+add_action('init', 'foody_api_user_role');
+
+function foody_custom_user_profile_fields($user)
+{
+    ?>
+    <h3>Extra profile information</h3>
+    <table class="form-table">
+        <tr>
+            <th>
+                <label for="type_of_writer">סוג כותב</label>
+            </th>
+            <td>
+                <select name="type_of_writer" id="type_of_writer">
+                    <option value="team">נבחרת</option>
+                    <option value="israel-cooks">בשלני ישראל</option>
+                </select>
+            </td>
+        </tr>
+    </table>
+    <!--    <table class="form-table">-->
+    <!--        <tr>-->
+    <!--            <th><label for="company">Company Name</label></th>-->
+    <!--            <td>-->
+    <!--                <input type="text" class="regular-text" name="company" value="--><?php //echo esc_attr( get_the_author_meta( 'company', $user->ID ) );
+    ?><!--" id="company" /><br />-->
+    <!--                <span class="description">Where are you?</span>-->
+    <!--            </td>-->
+    <!--        </tr>-->
+    <!--    </table>-->
+    <?php
+}
+
+add_action('user_new_form', 'foody_custom_user_profile_fields');
+
+function foody_save_custom_user_profile_fields($user_id)
+{
+    # again do this only if you can
+    if (!current_user_can('manage_options'))
+        return false;
+
+    # save my custom field
+    update_user_meta($user_id, 'type_of_writer', $_POST['type_of_writer']);
+}
+
+add_action('user_register', 'foody_save_custom_user_profile_fields');
+add_action('profile_update', 'foody_save_custom_user_profile_fields');
