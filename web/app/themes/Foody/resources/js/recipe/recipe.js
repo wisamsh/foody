c7ebcf63547a9cd185f87d46af40de0acbe0e911
Daniel Kissos <danielk@moveo.co.il>
2019-11-06 19:08:17 +0200

    add range for ratings

--- web/app/themes/Foody/resources/js/recipe/recipe.js
+++ web/app/themes/Foody/resources/js/recipe/recipe.js
@@ -125,6 +125,24 @@ if (foodyGlobals.post && (foodyGlobals.post.type == 'foody_recipe' || foodyGloba
 
 }
 
+jQuery(document).ready(($) => {
+    if($('.post-ratings')[0].innerText != ""){
+        $('.rating-digits').remove();
+    }
+
+    $('.post-ratings img').on('keypress',function () {
+        if( $('.rating-digits').length) {
+            $('.rating-digits').remove();
+        }
+    });
+
+    $('.post-ratings img').on('click',function () {
+        if( $('.rating-digits').length) {
+            $('.rating-digits').remove();
+        }
+    });
+});
+
 /**
  * Handle events and fire analytics dataLayer.push
  * @param event
