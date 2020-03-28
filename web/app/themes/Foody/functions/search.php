7a4bcd8935450ebc63e03e0c8e197940c0f4cf28
Daniel Kissos <danielk@moveo.co.il>
2020-03-28 11:02:05 +0300

    fix "2020/03/27 13:36:53 [error] 1399#1399: *80626 FastCGI sent in stderr: "PHP message: שגיאת מסד נתונים You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'י%' )"
    by escaping the search string

--- web/app/themes/Foody/functions/search.php
+++ web/app/themes/Foody/functions/search.php
@@ -110,6 +110,8 @@ function foody_search_user_by_name($name, $single = true)
 {
     global $wpdb;
 
+    $esc_name = esc_sql($name);
+
     $query =
         "SELECT user_id
               FROM $wpdb->usermeta 
@@ -118,11 +120,11 @@ function foody_search_user_by_name($name, $single = true)
               or ( meta_key='last_name' AND meta_value LIKE '%%%s%%' )";
 
     $args = [
-        $name,
-        $name
+        $esc_name,
+        $esc_name
     ];
 
-    $name_parts = explode(' ', trim($name));
+    $name_parts = explode(' ', trim($esc_name));
 
 
     if (count($name_parts) > 1) {
