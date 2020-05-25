fb4fe0864f58790771b7f4ca1a979fff09bdb862
Daniel Kissos <danielk@moveo.co.il>
2020-05-25 10:37:15 +0300

    fix if purchase btn dont have link

--- web/app/themes/Foody/template-parts/content-purchase-buttons.php
+++ web/app/themes/Foody/template-parts/content-purchase-buttons.php
@@ -15,27 +15,29 @@ $classes = $template_args['classes'];
 
 <ul class="purchase-buttons nolist row <?php echo $classes ?>">
 
-	<?php foreach ( $buttons as $button ) :
+    <?php foreach ($buttons as $button) :
 
-		$analytics_label = foody_get_array_default(
-			$button,
-			'analytics_label',
-			foody_get_array_default( $button, 'title', '' )
-		);
+        $analytics_label = foody_get_array_default(
+            $button,
+            'analytics_label',
+            foody_get_array_default($button, 'title', '')
+        );
 
 
-		$analytics_attr = '';
-		if ( ! empty( $analytics_label ) ) {
-			$analytics_attr = "data-analytics='$analytics_label'";
-		}
+        $analytics_attr = '';
+        if (!empty($analytics_label)) {
+            $analytics_attr = "data-analytics='$analytics_label'";
+        }
 
-		?>
+        if (isset($button['link']) && isset($button['link']['url']) && isset($button['link']['target'])){
+        ?>
         <li class="purchase-button-container col-6 col-lg-auto" <?php echo $analytics_attr ?>>
             <a href="<?php echo $button['link']['url'] ?>"
-               target="<?php echo ! empty( $button['link']['target'] ) ? $button['link']['target'] : '_blank' ?>">
-				<?php Foody_PurchaseButtons::the_button( $button ) ?>
+               target="<?php echo !empty($button['link']['target']) ? $button['link']['target'] : '_blank' ?>">
+                <?php Foody_PurchaseButtons::the_button($button) ?>
             </a>
         </li>
+    <?php } ?>
 	<?php endforeach; ?>
 
 </ul>
