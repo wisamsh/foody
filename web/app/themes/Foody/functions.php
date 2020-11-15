a8dd50402c8d07ee999595de33b45ded715e2bc5
Daniel Kissos <danielk@moveo.co.il>
2020-11-15 17:11:20 +0200

    try change social redirect to השלמת רישום

--- web/app/themes/Foody/functions.php
+++ web/app/themes/Foody/functions.php
@@ -887,6 +887,11 @@ function bit_recurring_fetch_transaction_status()
 
 add_action('init', 'bit_recurring_fetch_transaction_status');
 
+function redirect_social_login($user_id, $provider, $hybridauth_user_profile, $redirect_to){
+    $redirect_to = get_permalink(get_page_by_path('השלמת-רישום'));
+}
+add_action('wsl_hook_process_login_before_wp_safe_redirect', 'redirect_social_login');
+
 add_filter('body_class', 'foody_body_add_bit_class', 10, 1);
 function foody_body_add_bit_class($classes)
 {
