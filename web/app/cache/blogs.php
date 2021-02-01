b9e2c9c4372dc5c853846dbd2fd0585bc5db212f
Daniel Kissos <danielk@moveo.co.il>
2021-02-01 15:46:47 +0200

    add extra files

--- web/app/cache/blogs.php
+++ web/app/cache/blogs.php
@@ -1,1 +1,1 @@
-{"foody-local.co.il":"m1","white-label.co.il":"m3","foody.test":"m1","carine.foody-local.co.il":"m2"}
+{"foody-local.co.il":"m1","white-label.co.il":"m3","foody.test":"m1","daniel-test.foody-local.co.il":"m7"}
