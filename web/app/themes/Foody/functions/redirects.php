f4f8d4fc66bd0180832dea224014f2a7347e2d2b
Daniel Kissos <danielk@moveo.co.il>
2021-03-01 17:47:02 +0200

    fix wrong closing bracet

--- web/app/themes/Foody/functions/redirects.php
+++ web/app/themes/Foody/functions/redirects.php
@@ -50,7 +50,7 @@ function foody_logged_redirect() {
 	$slug = urldecode( get_post_field( 'post_name', get_post() ) );
 
 //	if ( (in_array( $slug, $signon_pages ) || is_home()) && is_user_logged_in()) {
-    if ( (in_array( $slug, $signon_pages ) || (is_front_page() && isset($_GET) && (isset($_GET['logister_popup']) || isset($_GET['redirect_to_provider']))) && is_user_logged_in())) {
+    if ( (in_array( $slug, $signon_pages ) || (is_front_page() && isset($_GET) && (isset($_GET['logister_popup']) || isset($_GET['redirect_to_provider'])))) && is_user_logged_in()) {
         if ( ! isset( $_REQUEST['registered']) ) {
 //			wp_redirect( home_url() );
             wp_redirect(get_permalink(get_page_by_path('השלמת-רישום')));
