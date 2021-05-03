4c2df2a83250e01c40111f364a4470c56838f17a
DavidV <davidv@moveo.co.il>
2021-05-03 15:13:01 +0300

    Fixing the rating bug

--- web/app/themes/Foody/resources/js/common/foody-rating.js
+++ web/app/themes/Foody/resources/js/common/foody-rating.js
@@ -1,7 +1,7 @@
 let FoodyLoader = require('../common/foody-loader');
 
 jQuery(document).ready(($) => {
-    // debugger;
+     // debugger;
     if (foodyGlobals.post && (foodyGlobals.post.type == 'foody_recipe' || foodyGlobals.post.type == 'post')) {
         if (foodyGlobals['can_user_rate'] &&
             ($('.ratings-wrapper .rating-stars-container .empty-star').length ||
@@ -14,10 +14,19 @@ jQuery(document).ready(($) => {
                         let starIndex = $(this).attr('data-index');
                         let topPracent = parentContainerIsWrapper ? 100 : 8;
                         let container = parentContainerIsWrapper ? '.ratings-wrapper' : '.comments-rating-prep-container .rating';
-                        let foodyLoader = new FoodyLoader({
-                            container: parentContainerIsWrapper ? $(container)[1] : $(container),
-                            id: 'rating-loader'
-                        });
+                        let foodyLoader;
+                        if ( $(window).width() < 768){
+                            foodyLoader = new FoodyLoader({
+                                container: $(container) ,
+                                id: 'rating-loader'
+                            });
+                        } else {
+                             foodyLoader = new FoodyLoader({
+                                container: parentContainerIsWrapper ? $(container)[1] : $(container),
+                                id: 'rating-loader'
+                            });
+                        }
+
 
                         foodyLoader.attach({topPercentage: topPracent});
                         foodyAjax({
