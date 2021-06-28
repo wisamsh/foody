e0710ccd418079d42db5b209828732744c0d0afb
Daniel Kissos <danielk@moveo.co.il>
2021-06-28 14:30:09 +0300

    add cover image (feed area) to articles

--- web/app/themes/Foody/inc/classes/class-foody-article.php
+++ web/app/themes/Foody/inc/classes/class-foody-article.php
@@ -25,4 +25,26 @@ class Foody_Article extends Foody_Post implements Foody_ContentWithSidebar {
 			]
 		);
 	}
+
+    public function before_content()
+    {
+        $cover_image = get_field('cover_image');
+        $mobile_image = get_field('mobile_cover_image');
+        $feed_area_id = get_field('recipe_channel');
+
+        if (isset($_GET['referer']) || $feed_area_id) {
+            $referer_post = isset($_GET['referer']) ? $_GET['referer'] : $feed_area_id;
+            if (!empty($referer_post)) {
+                $cover_image = get_field('cover_image', $referer_post);
+                $mobile_image = get_field('mobile_cover_image', $referer_post);
+            }
+        }
+
+        if (!empty($cover_image)) {
+            foody_get_template_part(get_template_directory() . '/template-parts/content-cover-image.php', [
+                'image' => $cover_image,
+                'mobile_image' => $mobile_image
+            ]);
+        }
+    }
 }
