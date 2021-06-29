56e8f8732c490d030515c2a6035de2f989b871d5
Daniel Kissos <danielk@moveo.co.il>
2021-06-29 18:00:37 +0300

    fix logo going over the breadcrumbs on items template

--- web/app/themes/Foody/inc/classes/class-foody-items-page.php
+++ web/app/themes/Foody/inc/classes/class-foody-items-page.php
@@ -9,11 +9,13 @@
 class Foody_ItemsPage {
 
 	public function cover() {
+	    $has_cover = false;
 		$image = get_field( 'cover_image' );
 		if ( ! empty( $image ) ) {
 			foody_get_template_part( get_template_directory() . '/template-parts/content-cover-image.php', $image );
+            $has_cover = true;
 		}
-
+		return $has_cover;
 	}
 
 	public function items() {
