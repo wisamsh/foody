8e4004cfa70af37e6bec227631ce2aa4133fbef2
DavidV <davidv@moveo.co.il>
2021-08-22 18:46:31 +0300

    Apply header.js on answer-template.php

---
+++ web/app/themes/Foody/resources/js/answer/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by moveosoftware on 6/27/18.
+ */
+window.EXIF = require('exif-js');
+jQuery(document).ready(function () {
+    require('./answer-analytics');
+    require('./answer-video');
+});
