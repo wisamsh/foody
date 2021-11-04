255b87e01e5526c56a46878368461cee18bbb601
Wisam Shomar <wisamsh@gmail.com>
2021-11-04 12:33:23 +0200

    Update facebook_meta_tags.php

--- web/app/themes/Foody/w_helpers/facebook_meta_tags.php
+++ web/app/themes/Foody/w_helpers/facebook_meta_tags.php
@@ -4,10 +4,11 @@
 "foody.co.il" => "242005383411645",
 "foody-local.co.il" => "123456"
 );
+$actual_link = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 ?>
 
 <meta property="fb:app_id"             content="<?php echo $domain[$_SERVER['HTTP_HOST']];?>" />
-<meta property="og:url"                content="https://foody.co.il" />
+<meta property="og:url"                content="<?php echo $actual_link ;?>" />
 <meta property="og:type"               content="article" />
 <meta property="og:title"              content="<?php echo get_the_title();?>" />
 <meta property="og:description"        content="פודי - המתכונים הכי טעימים של השפים והבלוגרים המובילים והכתבות הכי מעניינות. FOODY אתר האוכל הגדול בישראל - היכנסו לאתר לפרטים נוספים ובתאבון!" />
