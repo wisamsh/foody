6c5ff90a480ee8aa052b8a091fe2443126070b70
Wisam Shomar <wisamsh@gmail.com>
2021-11-18 11:58:24 +0200

    Create bit_api_tester.php

---
+++ web/app/themes/Foody/page-templates/bit_api_tester.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Bit Api Tester
+ *
+ * @package WordPress
+ * @subpackage Foody_WordPress
+ * @since Foody WordPress 1.0
+ */
+
+
+
+get_header();
+?>
+    <div id="main-content" class="main-content">
+
+        <div id="primary" class="content-area">
+            
+
+
+        </div><!-- #primary -->
+    </div><!-- #main-content -->
+
+
+<?php
+
+get_footer();
