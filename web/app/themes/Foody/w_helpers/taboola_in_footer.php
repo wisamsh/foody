01e67a8dba9a0d3470ba4fc1de1f6225923cbb8c
Wisam Shomar <wisamsh@gmail.com>
2022-01-31 14:28:10 +0200

    taboola atm 4

--- web/app/themes/Foody/w_helpers/taboola_in_footer.php
+++ web/app/themes/Foody/w_helpers/taboola_in_footer.php
@@ -5,6 +5,7 @@ $MySiteID =  (get_blog_details()->blog_id);
 if($MySiteID == 1 && get_post_type() == 'foody_recipe')
 {
 ?>
+<!--taboola_div-->
 <div id="taboola-below-article-thumbnails-1"></div>
 <script type="text/javascript">
   window._taboola = window._taboola || [];
@@ -16,7 +17,7 @@ if($MySiteID == 1 && get_post_type() == 'foody_recipe')
   });
 </script>
 
-<script type="text/javascript">
+<script id="taboola_footer" type="text/javascript">
   window._taboola = window._taboola || [];
   _taboola.push({flush: true});
 </script>
--- web/app/themes/Foody/w_helpers/taboola_in_head.php
+++ web/app/themes/Foody/w_helpers/taboola_in_head.php
@@ -1,27 +1,29 @@
-<?php 
-$MySiteID =  (get_blog_details()->blog_id); 
+<?php
+$MySiteID =  (get_blog_details()->blog_id);
 ?>
-<?php 
-if($MySiteID == 1 && get_post_type() == 'foody_recipe')
-{
+<?php
+if ($MySiteID == 1 && get_post_type() == 'foody_recipe') {
 ?>
-<script  type="text/javascript">
-  window._taboola = window._taboola || [];
-  _taboola.push({article:'auto'});
-  !function (e, f, u, i) :
-    if (!document.getElementById(i)){
+  <script id="taboola_head" type="text/javascript">
+    window._taboola = window._taboola || [];
+    _taboola.push({
+      article: 'auto'
+    });
+    ! function(e, f, u, i):
+    if (!document.getElementById(i)) {
       e.async = 1;
       e.src = u;
       e.id = i;
       f.parentNode.insertBefore(e, f);
     }
-  }(document.createElement('script'),
-  document.getElementsByTagName('script')[0],
-  '//cdn.taboola.com/libtrc/foody-foody/loader.js',
-  'tb_loader_script');
-  if(window.performance && typeof window.performance.mark == 'function')
-    {window.performance.mark('tbl_ic');}
-</script>
-<?php 
+    }(document.createElement('script'),
+      document.getElementsByTagName('script')[0],
+      '//cdn.taboola.com/libtrc/foody-foody/loader.js',
+      'tb_loader_script');
+    if (window.performance && typeof window.performance.mark == 'function') {
+      window.performance.mark('tbl_ic');
+    }
+  </script>
+<?php
 }
 ?>
