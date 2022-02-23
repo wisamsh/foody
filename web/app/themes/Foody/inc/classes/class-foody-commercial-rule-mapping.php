eeaf2c6ff4532ca95ff049cf2e7ca5dcd5724be0
Wisam Shomar <wisamsh@gmail.com>
2022-02-23 11:06:36 +0200

    Update class-foody-commercial-rule-mapping.php

--- web/app/themes/Foody/inc/classes/class-foody-commercial-rule-mapping.php
+++ web/app/themes/Foody/inc/classes/class-foody-commercial-rule-mapping.php
@@ -102,7 +102,7 @@ PRIMARY KEY (`ID`)) $charset_collate;";
             $sprtr .
             PHP_EOL .
 
-            print_r(array($wpdb), true)
+            print_r(array($wpdb->last_result), true)
         ));
 
 
