6d7845117a4568e1c3d68dd764884db694a0096a
Wisam Shomar <wisamsh@gmail.com>
2022-06-13 10:16:30 +0300

    ATM1

--- web/app/themes/Foody/template-parts/content-recipe-ingredients.php
+++ web/app/themes/Foody/template-parts/content-recipe-ingredients.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: moveosoftware
@@ -17,9 +18,21 @@ $ingredients_groups = $template_args['groups'];
 
 //Wisam===========================================================================================
 $Foody_Substitutes_Ingredients = new Foody_Substitutes_Ingredients(null);
+
+$Get_Common_Rule_Array = $Foody_Substitutes_Ingredients->Get_Common_Rule_For_Merchandise();
+//print_r($Get_Common_Rule_Array);
+
+//$term_id = get_term( 1151 )->term_id;
+
+
+//$tt = get_field("logo", "sponsors_" . $term_id);
+//echo ($tt['url']);
+
 echo $Foody_Substitutes_Ingredients->SwapStyle();
 
-//JS Scripts : 
+
+
+//JS Scripts :
 echo $Foody_Substitutes_Ingredients->swap_Script();
 //Wisam==========================================================================================
 
@@ -32,50 +45,92 @@ $recipe_id = isset($template_args['recipe_id']) ? $template_args['recipe_id'] : 
 
 
 
-<?php foreach ($ingredients_groups as $ingredients_group){ ?>
+<?php foreach ($ingredients_groups as $ingredients_group) { ?>
 
     <div class="col-12 ingredients-group p-0">
         <h2 class="ingredients-group-title">
-            <?php echo $ingredients_group['title']; 
-			
-			?>
+            <?php echo $ingredients_group['title'];
+
+            ?>
         </h2>
         <ul class="ingredients-group-list">
 
             <?php /** @var Foody_Ingredient $ingredient */
-			
-            foreach ($ingredients_group['ingredients'] as $ingredient){ ?>
+
+            //print_r($ingredients_group['ingredients']);
+            foreach ($ingredients_group['ingredients'] as $ingredient) { ?>
                 <?php
                 $id = "ingredient-" . $counter;
                 $counter++;
-				$TheSubIngPostID = '';
-				//Wisam Change Getting Substitute ingredients =======================================================================
-				if($ingredient->alternative_ing){
-					 $TheSubIngPostID =  $ingredient->alternative_ing;
-					 
-				}
-				$data_ing = $TheSubIngPostID ? "data-ingid=".$TheSubIngPostID : "";
-				$classIngSub = $data_ing ? "ing_wrapp" : "";
+                $TheSubIngPostID = '';
+                //Wisam Change Getting Substitute ingredients =======================================================================
+                if ($ingredient->alternative_ing) {
+                    $TheSubIngPostID = $ingredient->alternative_ing;
+                }
+                $ing_orginal_ID = $ingredient->id;
+                $data_ing = $TheSubIngPostID ? "data-ingid=" . $TheSubIngPostID : "";
+                $classIngSub = $data_ing ? "ing_wrapp" : "";
                 ?>
-                <li class="ingredients<?php echo " " . $classIngSub;?>" id="<?php echo $id; ?>" <?php echo $data_ing;?> 
-				<?php if($classIngSub){echo " style='overflow: initial !important;
-					max-height: 500px !important;'";}?>>
-                    <div class="ingredient Orginal_swapper_<?php echo $TheSubIngPostID;?>" id="ing_<?php echo $TheSubIngPostID;?>">
-					
-					
-					 <?php $ingredient->the_amounts(true, $recipe_id) ;
-					
-					 
-					 ?>
-						
+                <li class="ingredients<?php echo " " . $classIngSub; ?>" id="<?php echo $id; ?>" <?php echo $data_ing; ?> <?php if ($classIngSub) {
+                                                                                                                            echo " style='overflow: initial !important;max-height: 500px !important;'";
+                                                                                                                        }
+                                                                                                                        ?> data-orginal="<?php echo $ing_orginal_ID; ?>">
+
+
+
+
+                    <div class="ingredient Orginal_swapper_<?php echo $TheSubIngPostID; ?>" id="ing_<?php echo $TheSubIngPostID; ?>">
+
+
+                        <?php $ingredient->the_amounts(true, $recipe_id);
+
+
+                        ?>
+
+                        <?php //=========================Wisam Say : Getting @Common_Rule_Array ======================================================================
+
+                        //$ComRuleIngKey = array_search($ing_orginal_ID, $Get_Common_Rule_Array["ing"]);
+                        foreach ($Get_Common_Rule_Array["ing"] as $ComRuleIngKey => $ComRuleIngVal) {
+                            if ($ComRuleIngVal == $ing_orginal_ID) {
+                                $show_sponsor_text = $Get_Common_Rule_Array["show_sponsor_text"][$ComRuleIngKey];
+                                $show_sponsor_logo = $Get_Common_Rule_Array["show_sponsor_logo"][$ComRuleIngKey];
+                                $logo_url = $Get_Common_Rule_Array["logo_url"][$ComRuleIngKey];
+                                $sponser_Text = $Get_Common_Rule_Array["sponser_Text"][$ComRuleIngKey];
+                                $ing_link = $Get_Common_Rule_Array["ing_link"][$ComRuleIngKey];
+                                //echo $ComRuleIngKey;
+
+                                if (trim($show_sponsor_text) == 1) {
+                                    if ($ing_link) {
+                                        $ingr_URL = '<a target="_blank" href="' . $ing_link . '">' . $sponser_Text . '';
+                                    } else {
+                                        $ingr_URL = $sponser_Text;
+                                    }
+                                    //echo "<span class='sponser_Text'>".$ingr_URL."</span>";
+                                }
+
+                                if (trim($show_sponsor_logo == 1) && $logo_url != "") {
+
+                                    if ($ing_link) {
+                                        $Photo_comm_Url = '<a target="_blank" href="' . $ing_link . '"><img src="' . $logo_url . '"/></a>';
+                                    } else {
+                                        $Photo_comm_Url = '<img src="' . $logo_url . '"/>';
+                                    }
+                                    //echo "<span class='commorcial_logo milr'>".$Photo_comm_Url."</span>";
+                                }
+                            }
+                        }
+
+                        ?>
                     </div>
-					<?php 
-						 echo $Foody_Substitutes_Ingredients->get_text_image_Swap($TheSubIngPostID, get_the_ID());
-						 echo $Foody_Substitutes_Ingredients->get_Swap_Type( $TheSubIngPostID, get_the_ID() );
-						?>
-										
-                    <?php $ingredient->the_sponsored_ingredient(true, $recipe_id); ?>
-                    <?php echo $ingredient->get_substitute_ingredient($substitute_ingredients_filters, $recipe_id); ?>
+                    <?php
+                    echo $Foody_Substitutes_Ingredients->get_text_image_Swap($TheSubIngPostID, get_the_ID());
+                    echo $Foody_Substitutes_Ingredients->get_Swap_Type($TheSubIngPostID, get_the_ID());
+                    ?>
+
+                    <?php //$ingredient->the_sponsored_ingredient(true, $recipe_id); 
+                    ?>
+                    <?php //echo $ingredient->get_substitute_ingredient($substitute_ingredients_filters, $recipe_id); 
+                    ?>
                     <?php
                     // Add ingredient comment
                     if (!empty($ingredient->comment)) {
@@ -92,5 +147,3 @@ $recipe_id = isset($template_args['recipe_id']) ? $template_args['recipe_id'] : 
 
 
 <?php } ?>
-
-
