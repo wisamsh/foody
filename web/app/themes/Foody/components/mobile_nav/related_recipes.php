e44e774cc9178cb0de6d927c7d9137350fbddd34
Wisam Shomar <wisamsh@gmail.com>
2022-10-17 15:03:27 +0300

    atm

--- web/app/themes/Foody/components/mobile_nav/related_recipes.php
+++ web/app/themes/Foody/components/mobile_nav/related_recipes.php
@@ -1,81 +1,103 @@
 <span class="related-content-btn ">מתכונים נוספים</span>
 <div class="Conductor_overlay hidden"></div>
 <div class="related-recipes-container hidden">
-    
-	<div class="related_title">
-        מתכונים נוספים שכדאי לכם לנסות    </div>
-	<div class="close_related_btn">X</div>
-	
-	<?php 
+   
+<div class="related_title">
+מתכונים נוספים שכדאי לכם לנסות </div>
+<div class="close_related_btn">X</div>
+
+<?php
 $cat_id = 0;
 $list = array();
 $ids = array();
 
 
-	if(is_category()){
-		$cat_id = get_query_var('cat');
-		$list = array();
-		$ids = array();
-	}
+if(is_category()){
+$cat_id = get_query_var('cat');
+$list = array();
+$ids = array();
+}
 if(is_front_page()){
-	$cat_id = 0;
-	$ids = get_field("mobile_more_recipe_main_page", 'option');
+$cat_id = 0;
+$ids = get_field("mobile_more_recipe_main_page", 'option');
 }
 if('post'== get_post_type()){
-	$cat_id = 0;
-	$ids = get_field("mobile_more_recipe_articles", 'option');
+$cat_id = 0;
+$ids = get_field("mobile_more_recipe_articles", 'option');
 }
 
 if('foody_recipe'== get_post_type()){
-	$cat_cat = get_the_category();
-	$cat_id = $cat_cat[0]->term_id;
-	$ids=array();
+$cat_cat = get_the_category();
+$cat_id = $cat_cat[0]->term_id;
+$ids=array();
 
 
 }
 
 
-		  
+if('foody_feed_channel' == get_post_type()){
+$items= array();
+$blocks = get_field('blocks', get_the_ID());
+$item_Blocks=($blocks);
+
+
+
+foreach($item_Blocks as $n=>$k){
+if(is_array($k) && $k['type'] == 'manual'){
+
+$manualItems = $k['items'] ;
+
+foreach($manualItems as $j=>$v){
+$items[] = $v['post']->ID ;
+}
+
+
+}
+}
+}
+
+
+ 
 $args = array(
 
-	'post__in' => $ids ,		
-	'post_status' => 'publish',
-			'orderby' => 'post_date',
-			'order' => 'DESC',
-			'numberposts'      => 4,
-			'category'         => $cat_id,
-			'orderby'          => 'rand',
-			'post_type'        => 'foody_recipe',
-			'suppress_filters' => true,
+'post__in' => $ids ,
+'post_status' => 'publish',
+'orderby' => 'post_date',
+'order' => 'DESC',
+'numberposts' => 4,
+'category' => $cat_id,
+'orderby' => 'rand',
+'post_type' => 'foody_recipe',
+'suppress_filters' => true,
 
 );
-		$recipes = get_posts($args);
-		?>
-						<div class="container">
-						<div class="row">
-				<?php
-		
-		foreach($recipes as $r){
-			$img = get_the_post_thumbnail_url($r->ID);
-			$title= $r->post_title;
-			$lnk = get_permalink($r->ID);
-			?>
-				
-				<div class="colish">
-				<a href="<?php echo $lnk ;?>">
-				<img class="related_img" src="<?php echo $img;?>"/>
-				<div class="related_spn" ><?php echo $title;?></div>
-				</a>
-				</div>
-				
-			<?php
-		}
-		
-		?>
-				</div>
-				</div>
-		
-	
-	
-	
+$recipes = get_posts($args);
+?>
+<div class="container">
+<div class="row">
+<?php
+
+foreach($recipes as $r){
+$img = get_the_post_thumbnail_url($r->ID);
+$title= $r->post_title;
+$lnk = get_permalink($r->ID);
+?>
+
+<div class="colish">
+<a href="<?php echo $lnk ;?>">
+<img class="related_img" src="<?php echo $img;?>"/>
+<div class="related_spn" ><?php echo $title;?></div>
+</a>
+</div>
+
+<?php
+}
+
+?>
+</div>
+</div>
+
+
+
+
 </div>
