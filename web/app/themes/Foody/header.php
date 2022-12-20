4dd8bae5bbb06844404449c6977e9a3a9dd75d2e
Wisam Shomar <wisamsh@gmail.com>
2022-12-20 15:03:09 +0200

    Update header.php

--- web/app/themes/Foody/header.php
+++ web/app/themes/Foody/header.php
@@ -24,7 +24,7 @@ $show_accessibility = true;
 <head>
 <?php // for facebook metatags
 require (__DIR__ . '/w_helpers/facebook_meta_tags.php');
-require (__DIR__ . '/w_helpers/taboola_in_head.php');
+//require (__DIR__ . '/w_helpers/taboola_in_head.php');
 ?>
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
