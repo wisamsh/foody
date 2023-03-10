33f70ed8b156a73cf6b0e50659def1af3cac02b3
Wisam Shomar <wisamsh@gmail.com>
2023-03-10 15:54:12 +0200

    Update facebook_meta_tags.php

--- web/app/themes/Foody/w_helpers/facebook_meta_tags.php
+++ web/app/themes/Foody/w_helpers/facebook_meta_tags.php
@@ -19,5 +19,14 @@ $get_the_post_thumbnail_url = "https://foody-media.s3.eu-west-1.amazonaws.com/w_
 <meta property="og:title" content="<?php echo get_the_title();?>" />
 <meta property="og:description" content="פודי - המתכונים הכי טעימים של השפים והבלוגרים המובילים והכתבות הכי מעניינות. FOODY אתר האוכל הגדול בישראל - היכנסו לאתר לפרטים נוספים ובתאבון!" />
 <meta property="og:image" itemprop="image" content="<?php echo $get_the_post_thumbnail_url;?>" />
-
 <meta name="facebook-domain-verification" content="2g8c6oi7iii72tdemqqyr55bp9yey9" />
+<meta property="article:content_tier" content="free"/>
+
+
+
+<meta name="twitter:card" content="summary_large_image"/>
+<meta name="twitter:url" content="<?php echo $actual_link ;?>"/>
+<meta name="twitter:title" concontent="<?php echo get_the_title();?>" />
+<meta name="twitter:description" content="פודי - המתכונים הכי טעימים של השפים והבלוגרים המובילים והכתבות הכי מעניינות. FOODY אתר האוכל הגדול בישראל - היכנסו לאתר לפרטים נוספים ובתאבון!" />
+<meta name="twitter:image" content="<?php echo $get_the_post_thumbnail_url;?>" />
+
