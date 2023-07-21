4ddb7111cfde20f79e5805ad9c7e65039fa75b07
Wisam Shomar <wisamsh@gmail.com>
2023-07-21 09:31:36 +0300

    for the sake of it

--- web/app/themes/Foody/w_helpers/mehadrin.php
+++ web/app/themes/Foody/w_helpers/mehadrin.php
@@ -1,5 +1,4 @@
 <?php 
 $mehadrin_scrip_disable = '<script type="text/javascript" src="//acc.magixite.com/license/la?litk=guwoig46a1m"></script>';
  if($_SERVER['HTTP_HOST'] == "mehadrin.foody.co.il") {echo $mehadrin_scrip_disable ;}
- 
 ?>
