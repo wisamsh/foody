dc7afe259e0e10ca774f9473baf975e52c58f0b5
Emmanuel <manuelxtrem@gmail.com>
2023-10-20 15:34:59 +0000

    test

--- .github/script/env_setup.sh
+++ .github/script/env_setup.sh
@@ -12,13 +12,13 @@ echo "database host: $DB_HOST"
 
 # copy secrets to env
 # replace data in .env with value from secret
-sed -i '' "s/DB_NAME=database_name/DB_NAME=$DB_NAME/g" .env
+sed -i "s/DB_NAME=database_name/DB_NAME=$DB_NAME/g" .env
 
-sed -i '' "s/DB_USER=database_user/DB_USER=$DB_USER/g" .env
+sed -i "s/DB_USER=database_user/DB_USER=$DB_USER/g" .env
 
-sed -i '' "s/DB_PASSWORD=database_password/DB_PASSWORD=$DB_PASSWORD/g" .env
+sed -i "s/DB_PASSWORD=database_password/DB_PASSWORD=$DB_PASSWORD/g" .env
 
-sed -i '' "s/# DB_HOST=localhost/DB_HOST=$DB_HOST/g" .env
+sed -i "s/# DB_HOST=localhost/DB_HOST=$DB_HOST/g" .env
 
 # php setup
 
