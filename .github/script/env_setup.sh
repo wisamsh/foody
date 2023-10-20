07aefcecacbb4fe11f761524b96a5a06ebcfcee8
Emmanuel <manuelxtrem@gmail.com>
2023-10-20 15:22:14 +0000

    base setup

---
+++ .github/script/env_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# ensure .env file exists
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
+
+# copy secrets to env
+# replace data in .env with value from secret
+sed -i '' "s/DB_NAME=database_name/DB_NAME=$DB_NAME/g" .env
+
+sed -i '' "s/DB_USER=database_user/DB_USER=$DB_USER/g" .env
+
+sed -i '' "s/DB_PASSWORD=database_password/DB_PASSWORD=$DB_PASSWORD/g" .env
+
+sed -i '' "s/# DB_HOST=localhost/DB_HOST=$DB_HOST/g" .env
+
+# php setup
+
+# envoy setup
