3d7c818716f61aa1cabb3ea15b37a68c2abb5f88
Emmanuel <manuelxtrem@gmail.com>
2023-10-20 15:44:36 +0000

    composer fix

--- .github/script/env_setup.sh
+++ .github/script/env_setup.sh
@@ -5,12 +5,6 @@ if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
-echo "database name: $DB_NAME"
-echo "database user: $DB_USER"
-echo "database password: $DB_PASSWORD"
-echo "database host: $DB_HOST"
-
-# copy secrets to env
 # replace data in .env with value from secret
 sed -i "s/DB_NAME=database_name/DB_NAME=$DB_NAME/g" .env
 
@@ -20,6 +14,4 @@ sed -i "s/DB_PASSWORD=database_password/DB_PASSWORD=$DB_PASSWORD/g" .env
 
 sed -i "s/# DB_HOST=localhost/DB_HOST=$DB_HOST/g" .env
 
-# php setup
-
-# envoy setup
+cat .env
