5ee156661e22c462b33c5641166a627f0ec0b5af
Emmanuel <manuelxtrem@gmail.com>
2023-10-20 15:32:01 +0000

    test

--- .github/script/env_setup.sh
+++ .github/script/env_setup.sh
@@ -5,6 +5,11 @@ if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
+echo "database name: $DB_NAME"
+echo "database user: $DB_USER"
+echo "database password: $DB_PASSWORD"
+echo "database host: $DB_HOST"
+
 # copy secrets to env
 # replace data in .env with value from secret
 sed -i '' "s/DB_NAME=database_name/DB_NAME=$DB_NAME/g" .env
