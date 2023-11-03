75ad7362de0386d423e21ade23634aea9230c2e6
Wisam Shomar <wisamsh@gmail.com>
2023-11-03 15:53:51 +0200

    calories colore for happy food

---
+++ web/app/themes/Foody/template-parts/content-recipe-overview-happyfood.php
@@ -0,0 +1,171 @@
+
+<?php 
+$color_green = "#00800052";
+$color_yellow = "#d1bf0652";
+$color_red = "#fd090973";
+$clr = '';
+$clrMB = '';
+//mobile secssion ========================================================?>
+
+<div class="overview-lists-container">
+    <ul class="overview-with-icons row">
+        <?php foreach ($overview as $key => $value) : ?>
+            <?php if ($key !== 'difficulty_level') { 
+                if ($key == 'calories_per_dish' && (intval($value['text']) <= 30)){
+                    $clrMB=$color_green;
+                }
+                if ($key == 'calories_per_dish' && (intval($value['text']) < 31 && intval($value['text']) <= 45 )){
+                    $clrMB=$color_yellow;
+                }
+                if ($key == 'calories_per_dish' && (intval($value['text']) > 46)){
+                    $clrMB=$color_red;
+                }
+                ?>
+
+              
+
+                <li class="overview-item col-sm-3 col-3" style="background:<?php echo $clrMB;?>">
+                    <?php if ($key === 'time') { ?>
+                        <img src="<?php echo $GLOBALS['images_dir'] . 'icons/' . $value['preparation_time']['icon'] ?>">
+                        <div class="item-container">
+                            <div class="key-value">
+                                <span class="key"><?php echo $labels['preparation_time'] ?></span><?php echo ' ' . $value['preparation_time']['text'] ?>
+                            </div>
+                            <div class="key-value">
+                                <span class="key"><?php echo $labels['total_time'] ?></span><?php echo ' ' . $value['total_time']['text'] ?>
+                            </div>
+                        </div>
+                    <?php } else { ?>
+                        <img src="<?php echo $GLOBALS['images_dir'] . 'icons/' . $value['icon'] ?>">
+                        <div class="item-container">
+                            <div class="key">
+                                <?php echo $labels[$key] ?>
+                            </div>
+                            <div class="value <?php echo $key ?>">
+                                <?php echo $value['text'] ?>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </li>
+
+
+            <?php }
+        endforeach; ?>
+
+    </ul>
+    <ul class="overview-no-icons row">
+        <li class="overview-item col-sm-2 col-6">
+            <div class="difficulty-container">
+                <div class="key">
+                    <?php echo $labels['difficulty_level'] ?>
+                </div>
+                <div class="value <?php echo 'difficulty_level' ?>">
+                    <?php echo ' ' . $overview['difficulty_level']['text'] ?>
+                </div>
+            </div>
+        </li>
+        <li class="overview-item col-sm-2 col-6">
+            <div class="value kosher">
+                <?php echo __('כשר'); ?>
+            </div>
+        </li>
+    </ul>
+    <?php
+     if ( get_current_blog_id() !== 2 ) { ?>
+         <ul class="overview-nutrients row">
+             <li class="overview-item col-sm-1 col-6">
+                 <div class="value open">
+                     <?php echo __('עוד ערכים תזונתיים'); ?>
+                 </div>
+                 <section class="recipe-nutrition box no-print">
+
+                     <?php $recipe->the_nutrition() ?>
+
+                 </section>
+             </li>
+         </ul>
+    <?php } ?>
+</div>
+<?php // desktop secssion=======================================?>
+<div class="overview-lists-container-desktop">
+    <ul class="overview row">
+        <?php foreach ($overview as $key => $value) : ?>
+
+              
+            <?php if ($key !== 'difficulty_level') { 
+                
+                if ($key == 'calories_per_dish' && (intval($value['text']) <= 30)){
+                    $clr=$color_green;
+                }
+                if ($key == 'calories_per_dish' && (intval($value['text']) > 31 && intval($value['text']) <= 45 )){
+                    $clr=$color_yellow;
+                }
+                if ($key == 'calories_per_dish' && (intval($value['text']) > 46)){
+                    $clr=$color_red;
+                }
+        
+            
+                ?>
+                <li class="overview-item col-1" style="background:<?php echo $clr;?>">
+                    <?php if ($key === 'time') { ?>
+                        <img src="<?php echo $GLOBALS['images_dir'] . 'icons/' . $value['preparation_time']['icon-desktop'] ?>">
+                        <div class="item-container">
+                            <div class="key-value preparation-time">
+                                <span class="key"><?php echo $labels['preparation_time'] ?></span><?php echo ' ' . $value['preparation_time']['text'] ?>
+                            </div>
+                            <div class="key-value">
+                                <span class="key"><?php echo $labels['total_time'] ?></span><?php echo ' ' . $value['total_time']['text'] ?>
+                            </div>
+                        </div>
+                    <?php } else { ?>
+                        <img src="<?php echo $GLOBALS['images_dir'] . 'icons/' . $value['icon-desktop'] ?>">
+                        <div class="item-container">
+                            <div class="key">
+                                <?php echo $labels[$key] ?>
+                            </div>
+                            <div class="value <?php echo $key ?>">
+                                <?php echo $value['text'] ?>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </li>
+
+
+            <?php 
+            }
+        endforeach; ?>
+
+        <li class="overview-item difficulty col-1">
+            <div class="difficulty-container">
+                <div class="key">
+                    <?php echo $labels['difficulty_level'] ?>
+                </div>
+                <div class="value <?php echo 'difficulty_level' ?>">
+                    <?php echo ' ' . $overview['difficulty_level']['text'] ?>
+                </div>
+            </div>
+        </li>
+        <li class="overview-item col-1">
+            <div class="value kosher">
+                <span>
+                    <?php echo __('כשר'); ?>
+                </span>
+            </div>
+        </li>
+    </ul>
+    <?php
+    if ( get_current_blog_id() !== 2 ) { ?>
+        <ul class="overview-nutrients row">
+            <li class="overview-item col-sm-1 col-6">
+                <div class="value open">
+                    <?php echo __('עוד ערכים תזונתיים'); ?>
+                </div>
+                <section class="recipe-nutrition box no-print">
+
+                    <?php $recipe->the_nutrition() ?>
+
+                </section>
+            </li>
+        </ul>
+    <?php } ?>
+</div>
