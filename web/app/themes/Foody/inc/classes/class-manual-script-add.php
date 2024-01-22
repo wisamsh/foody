7efcb36e6533889024c396852b5e60f23e1639ad
Wisam Shomar <wisamsh@gmail.com>
2024-01-22 14:46:35 +0200

    atm1

--- web/app/themes/Foody/inc/classes/class-manual-script-add.php
+++ web/app/themes/Foody/inc/classes/class-manual-script-add.php
@@ -1,31 +1,43 @@
 <?php
 
 error_reporting(0);
-class ManualScriptAdd {
-   
+class ManualScriptAdd
+{
+
     public $print_the_script = array();
 
-    protected function ManualScript() {
+    protected function ManualScript()
+    {
         $manualScript = get_field("manual_site_script", "option");
-        foreach ($manualScript as $key=>$script) {
-            $area_zone = $script["script_area_type"];
-            $script_content = $script['script_content'];
-            $this->print_the_script[$key][$area_zone] = $script_content ;
+        foreach ($manualScript as $key => $script) {
+
+            if (
+                isset($script["script_area_type"]) && $script["script_area_type"] != trim("")
+                && $script["out_source"] != true
+            ) {
+                $area_zone = $script["script_area_type"];
+                $script_content = $script['script_content'];
+                
+                $this->print_the_script[$key][$area_zone] = $script_content;
+            }
+            if ($script["out_source"] == true && $script['out_source_code_Url'] != '') {
+                $area_zone = $script["script_area_type"];
+                $script_content = $script['out_source_code_Url'];
+                
+                $this->print_the_script[$key][$area_zone] = $script_content ;
+            }
         }
         return $this->print_the_script;
     }
 
-public function AddScriptToSite(){
-    $ScriptArray = $this->ManualScript();
-    
-    if(!empty( $ScriptArray )){
-        return $ScriptArray;
+    public function AddScriptToSite()
+    {
+        $ScriptArray = $this->ManualScript();
+
+        if (!empty($ScriptArray)) {
+            return $ScriptArray;
+        } else {
+            return "0";
+        }
     }
-   else{
-    return "0"; 
-   }
 }
-
-}
-
-?>
--- web/app/themes/Foody/w_helpers/foody-manual-scripts.php
+++ web/app/themes/Foody/w_helpers/foody-manual-scripts.php
@@ -6,18 +6,27 @@ if (is_array($Site_ManualScripts)) {
     foreach ($Site_ManualScripts as $key => $value) {
 
         foreach ($value as $page_key => $page_script) {
+            
 
             switch ($page_key) {
                 case (trim('home')):
                     if (is_front_page() || is_home()) {
-                        echo '<script>' . $page_script . '</script>';
+
+                       if(str_ends_with($page_script, '.js')){
+                        echo '<script src = "'. $page_script .'"></script>';
+                       }
+                        else{ echo '<script>' . $page_script . '</script>';}
+                  
                     }
 
                     break;
                 case (trim('recipe')):
 
                     if ('foody_recipe' === get_post_type()) {
-                        echo '<script>' . $page_script . '</script>';
+                        if(str_ends_with($page_script, '.js')){
+                            echo '<script src = "'. $page_script .'"></script>';
+                           }
+                            else{ echo '<script>' . $page_script . '</script>';}
                    
                     }
 
@@ -25,7 +34,10 @@ if (is_array($Site_ManualScripts)) {
 
                 case (trim('post')):
                     if ('post' === get_post_type()) {
-                       echo  '<script>' . $page_script . '</script>';
+                        if(str_ends_with($page_script, '.js')){
+                            echo '<script src = "'. $page_script .'"></script>';
+                           }
+                            else{ echo '<script>' . $page_script . '</script>';}
                     }
 
                     break;
