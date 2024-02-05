2c29f73710599202f708575f94e404f3bd265b77
Wisam Shomar <wisamsh@gmail.com>
2024-02-05 14:34:07 +0200

    atm1

--- web/app/themes/Foody/inc/classes/class-wsitemap.php
+++ web/app/themes/Foody/inc/classes/class-wsitemap.php
@@ -1,66 +1,86 @@
-<?php 
- class Foody_wsitemap 
+<?php
+class Foody_wsitemap
 {
-   
-private function get_posts_map_private($type){
-    
-  
+
+    private function get_posts_map_private($type, $xml = false)
+    {
+
         global $wpdb;
+        $custom_post_type = $type;
 
-
-    
-        $custom_post_type = $type; 
-    
         // A sql query to return all post titles
-        $results = $wpdb->get_results( $wpdb->prepare( "SELECT ID, post_title , post_name FROM {$wpdb->posts} WHERE post_type = %s and post_status = 'publish' order by ID desc ", $custom_post_type ), ARRAY_A );
-    
+        $results = $wpdb->get_results($wpdb->prepare("SELECT ID, post_title , post_date, post_name FROM {$wpdb->posts} WHERE post_type = %s and post_status = 'publish' order by ID desc ", $custom_post_type), ARRAY_A);
+
         // Return null if we found no results
-        if ( ! $results )
+        if (!$results)
             return;
-    
-        // HTML for our select printing post titles as loop
-        $output = '<ul style="list-style:square;">';
-    
-        foreach( $results as $index => $post ) {
-            $postID=$post['ID'];
-            $url = get_permalink($postID);
-            $output .= '<li  value="' . $post['ID'] . '"><a href="'.$url.'">' . $post['post_title'] . '</a></li>';
+        if ($xml == false) {
+            // HTML for our select printing post titles as loop
+            $output = '<ul style="list-style:square;">';
+
+            foreach ($results as $index => $post) {
+                $postID = $post['ID'];
+                $url = get_permalink($postID);
+                $output .= '<li  value="' . $post['ID'] . '"><a href="' . $url . '">' . $post['post_title'] . '</a></li>';
+            }
+
+            $output .= '</ul>'; // end of select element
+
+            // get the html
+            return $output;
         }
-    
-        $output .= '</ul>'; // end of select element
-    
-        // get the html
-        return $output;
-    
-    
 
+        if ($xml == true) {
+            // HTML for our select printing post titles as loop
+            $output = '';
 
-}
+            foreach ($results as $index => $post) {
+                $postID = $post['ID'];
+                $post_date = $post['post_date'];
+                $url = get_permalink($postID);
+                $output .= '<url>';
+                $output .= '<loc>'.htmlspecialchars($url);
+                $output .= '</loc>';
+                $output .= '<lastmod>'. $post_date ;
+                $output .= '</lastmod>';
+                $output .= '</url>';
+                
+            }
 
-public function get_posts_map($type){
-    return $this->get_posts_map_private($type);
-}
+           
 
-public function Do_FoodyBeadcrumbs()
-	{
-		echo '<section class="accessory-details-container">';
-		bootstrap_breadcrumb();
-		echo '</section>';
-	}
+            // get the html
+            return $output;
 
-public function get_the_tags(){
-    $AllTags = get_tags();
-    echo '<ul>';
-foreach($AllTags as $k=>$t){
-echo '<li><a href="/tag/'.$t->slug.'">' .$t->name . '</a></li>';
-}
-echo '</ul>';
-}
+        }
+    }
 
-public function MobileattrMap()
-	{
-		if (wp_is_mobile()) {
-			echo '<style> 
+    public function get_posts_map($type, $xml=false)
+    {
+        return $this->get_posts_map_private($type, $xml);
+    }
+
+    public function Do_FoodyBeadcrumbs()
+    {
+        echo '<section class="accessory-details-container">';
+        bootstrap_breadcrumb();
+        echo '</section>';
+    }
+
+    public function get_the_tags()
+    {
+        $AllTags = get_tags();
+        echo '<ul>';
+        foreach ($AllTags as $k => $t) {
+            echo '<li><a href="/tag/' . $t->slug . '">' . $t->name . '</a></li>';
+        }
+        echo '</ul>';
+    }
+
+    public function MobileattrMap()
+    {
+        if (wp_is_mobile()) {
+            echo '<style> 
 				#masthead{display:none;}
 				#content {
 				padding-top: 0px; 
@@ -73,20 +93,16 @@ public function MobileattrMap()
     margin-top: -10px;
                 }
 				</style>';
-		}
-	}
+        }
+    }
 
-public function get_the_sitemap_categories(){
-    $allcats = get_categories();
-    echo '<ul>';
-foreach($allcats as $k=>$t){
-echo '<li><a href="/category/'.$t->slug.'">' .$t->name . '</a></li>';
-}
-echo '</ul>';
-}
-
-
-
-
-
+    public function get_the_sitemap_categories()
+    {
+        $allcats = get_categories();
+        echo '<ul>';
+        foreach ($allcats as $k => $t) {
+            echo '<li><a href="/category/' . $t->slug . '">' . $t->name . '</a></li>';
+        }
+        echo '</ul>';
+    }
 } //END CLASS
---
+++ web/app/themes/Foody/page-templates/sitemap-xml.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: XML Wisam SiteMap
+ * 
+ */
+?>
+<?php 
+header('Content-Type: application/xml; charset=utf-8');
+
+//get_header();
+?>
+<?php
+echo'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+$foody_sitemap = new Foody_wsitemap();
+$FoodyPosts = $foody_sitemap->get_posts_map('post', true);
+$FoodyRecipes = $foody_sitemap->get_posts_map('foody_recipe', true);
+$questions = $foody_sitemap->get_posts_map('questions');
+$foody_accessory = $foody_sitemap->get_posts_map('foody_accessory', true);
+$foody_technique = $foody_sitemap->get_posts_map('foody_technique', true);
+$foody_ingredient = $foody_sitemap->get_posts_map('foody_ingredient', true);
+$foody_feed_channel = $foody_sitemap->get_posts_map('foody_feed_channel', true);
+$foody_sitemap->MobileattrMap();
+
+
+//$foody_filter = $foody_sitemap->get_posts_map('foody_filter');
+
+echo $FoodyRecipes;
+echo '</urlset>';
+?>
