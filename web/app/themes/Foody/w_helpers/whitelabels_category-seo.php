8f1112fc6e06ce1ca426691ae0cf03a312603b79
Wisam Shomar <wisamsh@gmail.com>
2024-02-29 13:34:02 +0200

    Update whitelabels_category-seo.php

--- web/app/themes/Foody/w_helpers/whitelabels_category-seo.php
+++ web/app/themes/Foody/w_helpers/whitelabels_category-seo.php
@@ -1,23 +1,30 @@
 <?php
 if (is_category()) {
-
-
-    function WhiteLabelMetaTags()
-    {
-        // Check if it's a specific page where you want to set noindex, nofollow
-        $meta_keywords_seo = get_field("meta_keywords_seo", get_the_ID());
-        $meta_tag_seo = get_field("meta_tag_seo", get_the_ID());
-        if (trim($meta_keywords_seo) != "") 
+    $current_category = get_queried_object();
+    
+    if (is_a($current_category, 'WP_Term') && $current_category->taxonomy == 'category') {
+        // The current queried object is a category
+        $category_id = $current_category->term_id;
+        $category_name = $current_category->name;
+    
+    
+        function WhiteLabelMetaTags()
         {
-            echo '<meta name="keywords" content="' . $meta_keywords_seo  . '" />';
+            // Check if it's a specific page where you want to set noindex, nofollow
+            $meta_keywords_seo = get_field("meta_keywords_seo",  $category_id);
+            $meta_tag_seo = get_field("meta_tag_seo",  $category_id);
+            if (trim($meta_keywords_seo) != "")
+            {
+                echo '<meta name="keywords" content="' . $meta_keywords_seo  . '" />';
+            }
+    
+            if (trim($meta_tag_seo) != "")
+            {
+                echo '<meta name="description" content="' . $meta_tag_seo . '" />';
+            }
         }
-
-        if (trim($meta_tag_seo) != "") 
-        {
-            echo '<meta name="description" content="' . $meta_tag_seo . '" />';
-        }
+    
+        // Hook into wp_head
+        add_action('wp_head', 'WhiteLabelMetaTags', 1);
     }
-
-    // Hook into wp_head
-    add_action('wp_head', 'WhiteLabelMetaTags', 1);
 }
