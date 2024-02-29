360f8b56cff934ffc7b5f3d081c62bbb8578f0b9
Wisam Shomar <wisamsh@gmail.com>
2024-02-29 11:18:50 +0200

    Update whitelabels_category-seo.php

--- web/app/themes/Foody/w_helpers/whitelabels_category-seo.php
+++ web/app/themes/Foody/w_helpers/whitelabels_category-seo.php
@@ -1,17 +1,23 @@
-<?php 
-if(is_category()){
-    
-   
-    function WhiteLabelMetaTags() {
-        // Check if it's a specific page where you want to set noindex, nofollow
-    $meta_keywords_seo = get_field("meta_keywords_seo", get_the_ID());
-    $meta_tag_seo = get_field("meta_tag_seo", get_the_ID());
-            echo '<meta name="keywords" content="'. $meta_keywords_seo  .' />';
-            echo '<meta name="description" content="'.$meta_tag_seo.'" />';
-        }
-        
-            // Hook into wp_head
-         add_action('wp_head', 'WhiteLabelMetaTags', 1);
-}
+<?php
+if (is_category()) {
 
-?>
+
+    function WhiteLabelMetaTags()
+    {
+        // Check if it's a specific page where you want to set noindex, nofollow
+        $meta_keywords_seo = get_field("meta_keywords_seo", get_the_ID());
+        $meta_tag_seo = get_field("meta_tag_seo", get_the_ID());
+        if (trim($meta_keywords_seo) != "") 
+        {
+            echo '<meta name="keywords" content="' . $meta_keywords_seo  . '" />';
+        }
+
+        if (trim($meta_tag_seo) != "") 
+        {
+            echo '<meta name="description" content="' . $meta_tag_seo . '" />';
+        }
+    }
+
+    // Hook into wp_head
+    add_action('wp_head', 'WhiteLabelMetaTags', 1);
+}
