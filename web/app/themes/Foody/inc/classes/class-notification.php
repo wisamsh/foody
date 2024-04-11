229eb479bb279e63c5654e210f05080cfd2a137f
Wisam Shomar <wisamsh@gmail.com>
2024-04-11 13:00:53 +0300

    Update class-notification.php

--- web/app/themes/Foody/inc/classes/class-notification.php
+++ web/app/themes/Foody/inc/classes/class-notification.php
@@ -1,25 +1,34 @@
-<?php 
-class Foody_Notification{
-
-function __construct()
+<?php
+class Foody_Notification
 {
-   $this->Creat_Necessary_Tables();
-  $this->enqueue_Notification_scripts();
- }
 
 
-function get_Details(){
-    print_r($_POST['email']);
-    exit;
-}
+    
+    function __construct()
+    {
+        $this->Creat_Necessary_Tables();
+        $this->enqueue_Notification_scripts();
+    }
+
+    public function not_icon()
+    {
+        return get_template_directory_uri() . '/resources/images/message_notification.png';
+    }
+    
+    function get_Details()
+    {
+        print_r($_POST);
+        exit;
+    }
 
 
 
-private function Creat_Necessary_Tables(){
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'notification_users'; // Your table name
-    $charset_collate = $wpdb->get_charset_collate();
-    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+    private function Creat_Necessary_Tables()
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'notification_users'; // Your table name
+        $charset_collate = $wpdb->get_charset_collate();
+        $sql = "CREATE TABLE IF NOT EXISTS $table_name (
             id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
             first_name VARCHAR(255) ,
             last_name VARCHAR(255) ,
@@ -34,73 +43,82 @@ private function Creat_Necessary_Tables(){
             PRIMARY KEY  (id)
         ) $charset_collate;";
 
-    $wpdb->query($sql);
-}
+        $wpdb->query($sql);
+    }
 
 
 
-public function get_Primary_Term(){
-    $Termrtn = [];
-    $foody_post = Foody_Post::create(get_post(), false);
-    $cat = $foody_post->get_primary_category();
-    $category = get_category($cat);
-    $term_id = ($category->term_id);
-    $term_Name = ($category->name);
-    $Termrtn['term_id'] =$term_id;
-    $Termrtn['term_Name'] =$term_Name;
-    return $Termrtn;
+    public function get_Primary_Term()
+    {
+        $Termrtn = [];
+        $foody_post = Foody_Post::create(get_post(), false);
+        $cat = $foody_post->get_primary_category();
+        $category = get_category($cat);
+        $term_id = ($category->term_id);
+        $term_Name = ($category->name);
+        $Termrtn['term_id'] = $term_id;
+        $Termrtn['term_Name'] = $term_Name;
+        return $Termrtn;
+    }
 
-}
 
+    public function DrawHTMLbox_notification()
+    {
+        $term = $this->get_Primary_Term();
 
-public function DrawHTMLbox_notification(){
-   $term = $this->get_Primary_Term();
-   
-   $rtn = '';
-    $rtn .= '<div class="notificationBox">';
-    $rtn .='<h4>שלחו לי התראה</h4>';
-    $rtn .= '<span>כשיש מתכון בקטגוריה : </span> <span><b>' .  $term['term_Name']   . '</b></span>';
-    $rtn .= '<form id="notification_form">
+        $rtn = '';
+        $rtn .= '<div class="notificationBox">';
+        $rtn .= '<img class="not_icon" src="' . $this->not_icon() . '"/>';
+        $rtn .= '<h4>שלחו לי התראה</h4>';
+        $rtn .= '<span>כשיש מתכון בקטגוריה : </span> <span><b>' .  $term['term_Name']   . '</b></span>';
+        $rtn .= '<form id="notification_form">
     <div class="formWrapper"><input type="email" name="email" id="email" class="not_email"/>
     <input type="submit" class="submit" value="שלח"></div>
     <input type="hidden" name="action" id="action" value="notification_action_call"/>
-    </form>';
-    $rtn .= '<p id="notification_ajax_response-response"></p></div>';
 
-    return $rtn;
-
-}
-
-public function enqueue_Notification_scripts() {
-    // Enqueue your script
-    wp_enqueue_script( 'notification-script', get_template_directory_uri() . '/components/js/notification.js', array(), '1.0.0', true );
-    wp_localize_script('notification-script', 'ajax_object', array('ajax_url' => admin_url('admin-ajax.php')));
-
-    add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_Notification_scripts' ) );
-   
-}
-
-
-public function handle_ajax() {
-    // Your AJAX handling logic here
-    $data = $_POST['email'];
-
-    // Process your data or perform any other actions
+    <input type="hidden" name="cat_id" id="cat_id" value="'.$term['term_id'] .'"/>
+    <input type="hidden" name="cat_name" id="cat_name" value="'.$term['term_Name'] .'"/>
+    <input type="hidden" name="recipe_id" id="recipe_id" value="'.get_the_ID() .'"/>
+    <input type="hidden" name="recipe_name" id="recipe_name" value="'.get_the_title() .'"/>
     
-    // Send a response (example response)
-    $response = 'Data received: ' . $data;
+    </form>';
+        $rtn .= '<p id="notification_ajax_response"></p></div>';
 
-    //wp_send_json_success($response); // Send success response
-   print_r( $response);
-}
+        return $rtn;
+    }
+
+    public function enqueue_Notification_scripts()
+    {
+        // Enqueue your script
+        wp_enqueue_script('notification-script', get_template_directory_uri() . '/components/js/notification.js', array(), '1.0.0', true);
+        wp_localize_script('notification-script', 'ajax_object', array('ajax_url' => admin_url('admin-ajax.php')));
+
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_Notification_scripts'));
+    }
+
+
+    public function handle_ajax()
+    {
+        // Your AJAX handling logic here
+        $data = $_POST;
+
+        // Process your data or perform any other actions
+
+        // Send a response (example response)
+        $response = 'Data received: ' . $data;
+
+        //wp_send_json_success($response); // Send success response
+        print_r($response);
+    }
 
 
 
 
 
 
-public function DrawCSS_Notification(){
-    $rtn = '
+    public function DrawCSS_Notification()
+    {
+        $rtn = '
     <style>
 .formWrapper{
     border: solid 1px #ddd;
@@ -110,8 +128,15 @@ public function DrawCSS_Notification(){
     border-radius: 5px;
     margin-top: 10px;
 }
+.not_icon{
+    width: 30px;
+    position: absolute;
+    left: 9px;
+    top: 3px;
+}
 
     .notificationBox{
+    position:relative;
     max-width: 400px;
     margin: 0 auto;
     text-align: right;
@@ -138,7 +163,6 @@ public function DrawCSS_Notification(){
     }
     </style>
     ';
-    return $rtn ;
-}
-
+        return $rtn;
+    }
 }
