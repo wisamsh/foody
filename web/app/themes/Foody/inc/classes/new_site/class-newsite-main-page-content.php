f559dd9cfd4b4c8777b4fa2fd9cb0ab64c077293
Wisam Shomar <wisamsh@gmail.com>
2024-10-25 17:46:22 +0300

    atm

--- web/app/themes/Foody/inc/classes/new_site/class-newsite-main-page-content.php
+++ web/app/themes/Foody/inc/classes/new_site/class-newsite-main-page-content.php
@@ -16,6 +16,26 @@ public function get_MainBanner(){
  }
 }
 
+public function Get_Promoted_Recipies(){
+    $posts_IDS =  get_field("ns_mp_featured_post");
+    $post_details = [];
+foreach($posts_IDS as $key=>$post_id){
+    $author_id = get_post_field('post_author', $post_id);
+    $post_details[$post_id] =  array(
+       "id"=>$post_id,
+        "title"=>get_the_title($post_id),
+        "expert"=>get_the_excerpt($post_id),
+        "featured_image"=> get_the_post_thumbnail_url($post_id, 'full'),
+        "author_name"=>get_the_author_meta('display_name', $author_id),
+        "author_avatar"=>get_avatar_url($author_id, ['size' => 96]),
+        "author_url"=>get_author_posts_url($author_id)
+    );
+}
+return $post_details;
+}
+
+
+
 
 }//END CLASS
 ?>
--- web/app/themes/Foody/template-parts/content-ns_mp_b1.php
+++ web/app/themes/Foody/template-parts/content-ns_mp_b1.php
@@ -3,5 +3,6 @@ $MainPageContent = new MainPageContent ;
 $get_MainBanner = $MainPageContent->get_MainBanner();
 ?>
 <section>
-    <div class="TopBannerMP"><img src="<?php echo $get_MainBanner ;?>"  class=""/></div>
+    <div class="TopBannerMP"><img src="<?php echo $get_MainBanner ;?>" loading="lazy" class=""/></div>
+<?php print_r($MainPageContent->Get_Promoted_Recipies());?>
 </section>
