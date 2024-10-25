bb2a9fe5f8755356059a68832f47ed4f576683bf
Wisam Shomar <wisamsh@gmail.com>
2024-10-25 17:02:57 +0300

    atm

--- web/app/themes/Foody/template-parts/content-ns_mp_b1.php
+++ web/app/themes/Foody/template-parts/content-ns_mp_b1.php
@@ -1,5 +1,7 @@
 <?php 
 $MainPageContent = new MainPageContent ;
 $get_MainBanner = $MainPageContent->get_MainBanner();
-echo ($get_MainBanner);
 ?>
+<section>
+    <div class="TopBannerMP"><img src="<?php echo $get_MainBanner ;?>"  class=""/></div>
+</section>
