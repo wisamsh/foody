6f5e1ee3928722b56b4db5b79633ff5719477c94
Wisam Shomar <wisamsh@gmail.com>
2024-12-13 13:05:30 +0200

    taboola area

--- web/app/themes/Foody/w_helpers/taboola_in_footer.php
+++ web/app/themes/Foody/w_helpers/taboola_in_footer.php
@@ -3,12 +3,13 @@
   $site_id = get_current_blog_id();
 ?>
 <?php 
-if ($site_id == 1 ||  $_SERVER['HTTP_HOST'] == "staging.foody.co.il") {
+if ($site_id == 1 ||  $_SERVER['HTTP_HOST'] == "staging.foody.co.il" || $_SERVER['HTTP_HOST'] == "foody.co.il") {
 
 ?>
 <!--taboola_div-->
-
+<div style="width:960px;margin:0 auto; position:relative" id="wt_ab">
 <div id="taboola-below-article-thumbnails-1"></div>
+</div>
 <script type="text/javascript">
 window._taboola = window._taboola || [];
 _taboola.push({
