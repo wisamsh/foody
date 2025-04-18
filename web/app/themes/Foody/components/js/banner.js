635122290bf0d7d73508a07c5d58ed651c10cd1b
Wisam Shomar <wisamsh@gmail.com>
2025-04-18 16:15:11 +0300

    atm

--- web/app/themes/Foody/components/js/banner.js
+++ web/app/themes/Foody/components/js/banner.js
@@ -2,3 +2,8 @@ jQuery(".close_banner").on('click', function(e){
 IDToClose = (jQuery(this).attr('data-close'));
 jQuery("#" + IDToClose ).toggle();
 });
+
+jQuery(".banner_wrapper").on("click", function(){
+linkURI = (jQuery(this).attr('data-url'));
+window.location = linkURI  ; 
+});
--- web/app/themes/Foody/components/js/dist/banner.dev.js
+++ web/app/themes/Foody/components/js/dist/banner.dev.js
@@ -4,3 +4,7 @@ jQuery(".close_banner").on('click', function (e) {
   IDToClose = jQuery(this).attr('data-close');
   jQuery("#" + IDToClose).toggle();
 });
+jQuery(".banner_wrapper").on("click", function () {
+  linkURI = jQuery(this).attr('data-url');
+  window.location = linkURI;
+});
